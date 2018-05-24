57fac730e9f84d4b652d9b35ff9ef630e853e850
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-05-24 11:52:12 +0100

    Log warning when StorageException is thrown

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/info/BlobContainerInfoContributor.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/info/BlobContainerInfoContributor.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.info.Info.Builder;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class BlobContainerInfoContributor implements InfoContributor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlobContainerInfoContributor.class);
     private final CloudBlobContainer container;
 
     public BlobContainerInfoContributor(CloudBlobContainer container) {
@@ -29,6 +32,7 @@ public class BlobContainerInfoContributor implements InfoContributor {
         try {
             return container.exists();
         } catch (StorageException e) {
+            LOGGER.warn("Could not check if container exists", e);
             return false;
         }
     }
