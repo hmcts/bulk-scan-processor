c09f46f9d3a8ac87e0328ca19b46c1aacf3f403c
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-05-25 11:32:20 +0100

    Differentiate storage accounts between environments
    Each environment should have its own account and storage.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "provider" {
-  name                     = "bulkscanning"
+  name                     = "bulkscanning-${var.env}"
   resource_group_name      = "${azurerm_resource_group.rg.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
