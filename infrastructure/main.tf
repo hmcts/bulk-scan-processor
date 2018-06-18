22a79498644b229ff639bce3254f4bde922f37d7
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-06-18 08:57:19 +0100

    Enable https traffic only

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,12 +40,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "provider" {
-  name                     = "bulkscanning${var.env}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  location                 = "${var.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "BlobStorage"
+  name                      = "bulkscanning${var.env}"
+  resource_group_name       = "${azurerm_resource_group.rg.name}"
+  location                  = "${var.location}"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "BlobStorage"
+  enable_https_traffic_only = true
 }
 
 resource "azurerm_storage_container" "sscs" {
