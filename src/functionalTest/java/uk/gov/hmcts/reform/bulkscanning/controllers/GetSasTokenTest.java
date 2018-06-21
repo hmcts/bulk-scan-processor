11f5da5b77fd2daa06997d409f1b76738a3fce2d
nitinprabhu <prabhunitin61@gmail.com>
2018-06-21 16:01:53 +0100

    Removed unnecessary annotations

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanning/controllers/GetSasTokenTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanning/controllers/GetSasTokenTest.java
@@ -9,8 +9,6 @@ import org.assertj.core.util.DateUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,8 +19,6 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class GetSasTokenTest {
 
     @Value("${test-url}")
