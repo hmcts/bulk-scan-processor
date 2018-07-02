83b834fb43c4196252aad4421e7bae2f007d7296
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-07-02 11:34:02 +0100

    Schedule reading scans

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/BlobStorageRead.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/BlobStorageRead.java
@@ -10,6 +10,7 @@ import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -33,6 +34,7 @@ public class BlobStorageRead {
         this.cloudBlobClient = cloudBlobClient;
     }
 
+    @Scheduled(fixedDelayString = "${scan.interval}")
     public void readBlobs() {
         cloudBlobClient.listContainers().forEach(cloudBlobContainer -> {
             try {
