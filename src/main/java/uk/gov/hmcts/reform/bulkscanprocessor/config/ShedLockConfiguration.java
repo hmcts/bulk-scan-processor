dfcc5e4b46fe4d72b8aaeef6c61ec35495481cd4
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-07-05 16:14:42 +0100

    Ensure that ShedLock is configured after Flyway

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ShedLockConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ShedLockConfiguration.java
@@ -4,13 +4,17 @@ import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbc.JdbcLockProvider;
 import net.javacrumbs.shedlock.spring.ScheduledLockConfiguration;
 import net.javacrumbs.shedlock.spring.ScheduledLockConfigurationBuilder;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import java.time.Duration;
 import javax.sql.DataSource;
 
 @Configuration
+@AutoConfigureAfter(FlywayConfiguration.class)
+@DependsOn({"flyway", "flywayInitializer"})
 public class ShedLockConfiguration {
 
     @Bean
