8df3c4ee0d561dca54ce4ddd010c0ef06d664c76
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-07-06 15:35:08 +0100

    Enable app insights

--- src/smokeTest/java/uk/gov/hmcts/reform/bulkscanprocessor/WhenApplicationDeployedTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscanprocessor/WhenApplicationDeployedTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.logging.appinsights.SyntheticHeaders;
 
 import static org.hamcrest.Matchers.emptyArray;
 import static org.hamcrest.Matchers.not;
@@ -23,6 +24,7 @@ public class WhenApplicationDeployedTest {
             .given()
             .relaxedHTTPSValidation()
             .baseUri(testUrl)
+            .header(SyntheticHeaders.SYNTHETIC_TEST_SOURCE, "Bulk Scan Processor smoke test")
             .get("/info")
             .then()
             .body("containers", not(emptyArray()));
