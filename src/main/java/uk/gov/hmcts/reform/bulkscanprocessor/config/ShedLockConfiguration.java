f8641fa72100668dbbfe78762b7ae79d4f7a086b
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-07-12 15:08:58 +0100

    Distributed lock (#30)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ShedLockConfiguration.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbc.JdbcLockProvider;
+import net.javacrumbs.shedlock.spring.ScheduledLockConfiguration;
+import net.javacrumbs.shedlock.spring.ScheduledLockConfigurationBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import java.time.Duration;
+import javax.sql.DataSource;
+
+@Configuration
+@AutoConfigureAfter(FlywayConfiguration.class)
+@DependsOn({"flyway", "flywayInitializer"})
+public class ShedLockConfiguration {
+
+    @Value("${scheduling.pool}")
+    private int poolSize;
+
+    @Value("${scheduling.lock_at_most_for}")
+    private String lockAtMostFor;
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcLockProvider(dataSource);
+    }
+
+    @Bean
+    public ScheduledLockConfiguration taskScheduler(LockProvider lockProvider) {
+        return ScheduledLockConfigurationBuilder
+            .withLockProvider(lockProvider)
+            .withPoolSize(poolSize)
+            .withDefaultLockAtMostFor(Duration.parse(lockAtMostFor))
+            .build();
+    }
+}
