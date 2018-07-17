9aea7b6e898bddc760dc0f3fbfd1047debe7089a
nitinprabhu <prabhunitin61@gmail.com>
2018-07-17 13:56:25 +0100

    Fixed integration tests

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/AuthServiceConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/AuthServiceConfig.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.config;
 
+import org.apache.commons.lang.NotImplementedException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,9 @@ import org.springframework.context.annotation.Lazy;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+
+import java.util.List;
 
 @Configuration
 @Lazy
@@ -21,5 +26,23 @@ public class AuthServiceConfig {
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, name, serviceAuthorisationApi);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "idam.s2s-auth.url", havingValue = "false")
+    public AuthTokenValidator tokenValidatorStub() {
+        return new AuthTokenValidator() {
+            public void validate(String token) {
+                throw new NotImplementedException();
+            }
+
+            public void validate(String token, List<String> roles) {
+                throw new NotImplementedException();
+            }
+
+            public String getServiceName(String token) {
+                return "some_service_name";
+            }
+        };
     }
 }
