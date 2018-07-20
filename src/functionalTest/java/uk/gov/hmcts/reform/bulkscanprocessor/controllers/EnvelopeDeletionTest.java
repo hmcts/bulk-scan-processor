8842d9218e70de75462e9511eddb99b8c1d88f3a
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-07-20 14:15:37 +0100

    Rename test name to EnvelopeDeletionTest

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/EnvelopeDeletionTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/EnvelopeDeletionTest.java
@@ -20,7 +20,7 @@ import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource("classpath:application.properties")
-public class WhenProcessingEnvelopeTest {
+public class EnvelopeDeletionTest {
 
     @Value("${test-url}")
     private transient String testUrl;
