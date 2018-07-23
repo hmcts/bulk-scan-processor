409203c13d92dac64b4faafb6a8523fe18483652
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-07-23 09:35:28 +0100

    Delete unused exception

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/InvalidEnvelopeException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
-
-public class InvalidEnvelopeException extends RuntimeException {
-
-    public InvalidEnvelopeException(String message) {
-        super(message);
-    }
-}
