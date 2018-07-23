697b5783f250fda763972807d9dc79bc84501065
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-07-23 12:09:54 +0100

    Fix typo: Add missing closing curly brace in var name

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/EnvelopeDeletionTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/EnvelopeDeletionTest.java
@@ -28,7 +28,7 @@ public class EnvelopeDeletionTest {
     @Value("${scan-delay}")
     private transient int scanDelay;
 
-    @Value("${storage-account-name")
+    @Value("${storage-account-name}")
     private transient String accountName;
 
     private transient CloudBlobContainer testContainer;
