001599f3f3472e4af0da4e656883accc0d8a769e
Rafal Kondratowicz <rkondr@gmail.com>
2018-07-26 17:14:37 +0100

    Suppress irrelevant warnings

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/EnvelopeUpdateServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/EnvelopeUpdateServiceTest.java
@@ -22,6 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
+@SuppressWarnings("PMD.BeanMembersShouldSerialize")
 @RunWith(MockitoJUnitRunner.class)
 public class EnvelopeUpdateServiceTest {
 
