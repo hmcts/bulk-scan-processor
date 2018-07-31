d73aa312341b13eaa572561cc6d8bc19cd3001f2
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-07-31 14:42:34 +0100

    RPE-556 Added annotation for json deserialization

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/EnvelopeMetadataResponse.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/EnvelopeMetadataResponse.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.model.out;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
 
@@ -10,6 +11,7 @@ public class EnvelopeMetadataResponse {
     @JsonProperty("envelopes")
     public final List<Envelope> envelopes;
 
+    @JsonCreator
     public EnvelopeMetadataResponse(List<Envelope> envelopes) {
         this.envelopes = envelopes;
     }
