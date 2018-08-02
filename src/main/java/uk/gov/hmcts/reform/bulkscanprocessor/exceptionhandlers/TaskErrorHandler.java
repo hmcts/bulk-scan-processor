9a099ac3af80b90959065285296ef03f60dcc0c7
donatas M <doncem@users.noreply.github.com>
2018-08-02 13:35:32 +0100

    Feature: Custom task scheduler (#58)
    * Plug-in custom ErrorHandler
    
    * Propose error handler implementation
    
    * Create custom task scheduler
    
    * Move class level arguments to bean construct
    
    * Fix CloudBlocClient Bean configuration for test
    
    * Simplify shedlock test
    
    * Rename interface to *throwable
    
    * Add logging of state/event changes

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptionhandlers/TaskErrorHandler.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.exceptionhandlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ErrorHandler;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEvent;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEventRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.Status;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.EnvelopeAwareThrowable;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.EventRelatedThrowable;
+
+public class TaskErrorHandler implements ErrorHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(TaskErrorHandler.class);
+
+    private final EnvelopeRepository envelopeRepository;
+
+    private final ProcessEventRepository eventRepository;
+
+    public TaskErrorHandler(EnvelopeRepository envelopeRepository, ProcessEventRepository eventRepository) {
+        this.envelopeRepository = envelopeRepository;
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public void handleError(Throwable t) {
+        if (t instanceof EnvelopeAwareThrowable) {
+            updateEnvelopeLastStatus((EnvelopeAwareThrowable) t);
+        }
+
+        if (t instanceof EventRelatedThrowable) {
+            registerEvent((EventRelatedThrowable) t, t.getMessage());
+        }
+
+        log.error(t.getMessage(), t);
+    }
+
+    private void updateEnvelopeLastStatus(EnvelopeAwareThrowable throwable) {
+        Status.fromEvent(throwable.getEvent()).ifPresent(status -> {
+            Envelope envelope = throwable.getEnvelope();
+            envelope.setStatus(status);
+
+            envelopeRepository.save(envelope);
+
+            log.info(
+                "Change envelope {} from {} and {} status to {}",
+                envelope.getId(),
+                envelope.getContainer(),
+                envelope.getZipFileName(),
+                status
+            );
+        });
+    }
+
+    private void registerEvent(EventRelatedThrowable exception, String reason) {
+        ProcessEvent processEvent = new ProcessEvent(
+            exception.getContainer(),
+            exception.getZipFileName(),
+            exception.getEvent()
+        );
+
+        processEvent.setReason(reason);
+        eventRepository.save(processEvent);
+
+        log.info(
+            "Zip {} from {} marked as {}",
+            processEvent.getZipFileName(),
+            processEvent.getContainer(),
+            processEvent.getEvent()
+        );
+    }
+}
