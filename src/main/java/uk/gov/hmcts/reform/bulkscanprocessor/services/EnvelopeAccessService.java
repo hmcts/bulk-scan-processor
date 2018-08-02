d199ecd1c6689d4a5ade7a32335a2b15bd41ea8a
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-08-02 09:52:12 +0100

    Move envelope access related actions to it's service

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/EnvelopeAccessService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/EnvelopeAccessService.java
@@ -1,21 +1,37 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.services;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.EnvelopeAccessProperties;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.EnvelopeAccessProperties.Mapping;
 import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.ForbiddenException;
 import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.ServiceConfigNotFoundException;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.ServiceJuridictionConfigNotFoundException;
 
 import java.util.List;
 import java.util.Objects;
 
 @Service
+@EnableConfigurationProperties(EnvelopeAccessProperties.class)
 public class EnvelopeAccessService {
 
     private final List<Mapping> mappings;
 
     public EnvelopeAccessService(EnvelopeAccessProperties accessProps) {
         this.mappings = accessProps.getMappings();
+    }
+
+    public String getJurisdictionByServiceName(String serviceName) {
+        return mappings
+            .stream()
+            .filter(m -> Objects.equals(m.getReadService(), serviceName))
+            .findFirst()
+            .map(Mapping::getJurisdiction)
+            .orElseThrow(() ->
+                new ServiceJuridictionConfigNotFoundException(
+                    "No configuration mapping found for service " + serviceName
+                )
+            );
     }
 
     /**
