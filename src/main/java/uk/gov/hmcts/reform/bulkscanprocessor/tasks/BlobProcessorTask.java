ebaace8942c5e977f38ccff26e529f570838b0a5
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-08-06 09:34:02 +0100

    Move scan delay config under the scheduling

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/BlobProcessorTask.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/BlobProcessorTask.java
@@ -60,7 +60,7 @@ public class BlobProcessorTask {
     }
 
     @SchedulerLock(name = "blobProcessor")
-    @Scheduled(fixedDelayString = "${scan.delay}")
+    @Scheduled(fixedDelayString = "${scheduling.task.scan.delay}")
     public void processBlobs() throws IOException, StorageException, URISyntaxException {
         for (CloudBlobContainer container : cloudBlobClient.listContainers()) {
             processZipFiles(container);
