2b0a1b04f8637c401cb7ab2f4f32725bab570206
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-08-10 16:57:33 +0100

    RPE-610 Clarified serialization/deserialization intent

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/Classification.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/Classification.java
@@ -10,6 +10,17 @@ public enum Classification {
 
     private final String value;
 
+    /**
+     * We accept mixed cases here as that does not change the value semantically
+     * and this allows us to be permissive in what we accept.
+     *
+     * When a classification value is instead serialized to Json only lowercase
+     * is used as the corresponding field in the external model is a String which
+     * is populated using the toString() method in this class (see EnvelopeResponse).
+     * This means that we are strict with what we send.
+     *
+     * @param value
+     */
     Classification(final String value) {
         this.value = (Strings.isNullOrEmpty(value) ? null : value.toLowerCase());
     }
