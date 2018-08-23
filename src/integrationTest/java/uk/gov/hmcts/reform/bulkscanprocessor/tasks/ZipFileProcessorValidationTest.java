a1a275e52904c0f094226111ccbfbdfaea05f865
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-08-23 11:25:51 +0100

    Just use basic unit test suite

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ZipFileProcessorValidationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ZipFileProcessorValidationTest.java
@@ -1,15 +1,7 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
 
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
-import org.junit.After;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.springframework.boot.test.rule.OutputCapture;
-import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.DocUploadFailureGenericException;
 import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.FileNameIrregularitiesException;
-import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.NonPdfFileFoundException;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.wrapper.ErrorHandlingWrapper;
 import uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor.ZipFileProcessor;
 import uk.gov.hmcts.reform.bulkscanprocessor.util.EntityParser;
@@ -19,7 +11,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.zip.ZipInputStream;
 
 import static com.google.common.io.Resources.getResource;
@@ -29,11 +20,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 /**
  * This is unit test. Falls under integration to make use of existing zip file resources.
  */
-@RunWith(Parameterized.class)
 public class ZipFileProcessorValidationTest {
-
-    @Rule
-    public OutputCapture capture = new OutputCapture();
 
     private static final Processor processor = new Processor(
         null,
@@ -45,74 +32,55 @@ public class ZipFileProcessorValidationTest {
     ) {
     };
 
-    @Parameterized.Parameters(name = "Processing {0}. Should fail on custom validator? {2}")
-    public static Iterable<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-            { "1_24-06-2018-00-00-00.zip", 2, false, NullPointerException.class },// valid, can't call dm
-            { "2_24-06-2018-00-00-00.zip", 2, false, null },// invalid, zip has folder, no metadata file
-            { "3_24-06-2018-00-00-00.zip", 1, false, null },// invalid, no metadata file
-            { "4_24-06-2018-00-00-00.zip", 1, true, FileNameIrregularitiesException.class },// incorrect # files
-            { "5_24-06-2018-00-00-00.zip", 2, false, null },// contains image
-            { "6_24-06-2018-00-00-00.zip", 1, false, null },// invalid metadata file - missing deliverydate
-            { "7_24-06-2018-00-00-00.zip", 1, false, NullPointerException.class },// valid, can't call dm
-            { "8_24-06-2018-00-00-00.zip", 1, true, FileNameIrregularitiesException.class }// incorrect # files
-        });
-    }
+    @Test
+    public void should_throw_exception_when_zip_file_contains_fewer_pdfs() throws IOException, URISyntaxException {
+        ZipFileProcessor zipFileProcessor = getZipFileProcessor("4_24-06-2018-00-00-00.zip");
 
-    private final String zipFileName;
+        Throwable throwable = catchThrowable(() ->
+            processor.processParsedEnvelopeDocuments(
+                zipFileProcessor.getEnvelope(),
+                zipFileProcessor.getPdfs(),
+                null
+            )
+        ).getCause();
 
-    private final int pdfCount;
-
-    private final boolean whenExpectingMissingPdfs;
-
-    private final Class<? extends Throwable> actualException;
-
-    public ZipFileProcessorValidationTest(
-        String zipFileName,
-        int pdfCount,
-        boolean whenExpectingMissingPdfs,
-        Class<? extends Throwable> actualException
-    ) {
-        this.zipFileName = zipFileName;
-        this.pdfCount = pdfCount;
-        this.whenExpectingMissingPdfs = whenExpectingMissingPdfs;
-        this.actualException = actualException;
-    }
-
-    @After
-    public void tearDown() {
-        capture.flush();
+        assertThat(throwable).isInstanceOf(FileNameIrregularitiesException.class)
+            .hasMessageMatching("Missing PDFs: 1111001.pdf");
     }
 
     @Test
-    public void should_throw_relevant_exception_when_zip_file_is_invalid() throws IOException, URISyntaxException {
-        ZipFileProcessor zipFileProcessor = getZipFileProcessor(zipFileName);
+    public void should_throw_exception_when_zip_file_contains_more_pdfs() throws IOException, URISyntaxException {
+        ZipFileProcessor zipFileProcessor = getZipFileProcessor("9_24-06-2018-00-00-00.zip");
 
-        if (actualException != null) {
-            assertThat(capture.toString()).containsPattern(".+ INFO  \\[.+\\] "
-                + ZipFileProcessor.class.getCanonicalName() + ":\\d+: "
-                + "PDFs found in " + zipFileName + ": " + pdfCount
-            );
+        Throwable throwable = catchThrowable(() ->
+            processor.processParsedEnvelopeDocuments(
+                zipFileProcessor.getEnvelope(),
+                zipFileProcessor.getPdfs(),
+                null
+            )
+        ).getCause();
 
-            Throwable throwable = catchThrowable(() ->
-                processor.processParsedEnvelopeDocuments(
-                    zipFileProcessor.getEnvelope(),
-                    zipFileProcessor.getPdfs(),
-                    null
-                )
-            ).getCause();
-
-            if (whenExpectingMissingPdfs) {
-                assertThat(throwable).isInstanceOf(actualException)
-                    .hasMessageMatching("Missing PDFs: .+\\.(pdf|jpg|gif).*");
-            } else {
-                assertThat(throwable).isInstanceOf(DocUploadFailureGenericException.class)
-                    .hasCauseExactlyInstanceOf(actualException);
-            }
-        }
+        assertThat(throwable).isInstanceOf(FileNameIrregularitiesException.class)
+            .hasMessageMatching("Missing PDFs: 1111002.pdf");
     }
 
-    private ZipFileProcessor getZipFileProcessor(String fileName) throws IOException, URISyntaxException {
+    @Test
+    public void should_throw_exception_when_zip_file_has_mismatching_pdf() throws IOException, URISyntaxException {
+        ZipFileProcessor zipFileProcessor = getZipFileProcessor("8_24-06-2018-00-00-00.zip");
+
+        Throwable throwable = catchThrowable(() ->
+            processor.processParsedEnvelopeDocuments(
+                zipFileProcessor.getEnvelope(),
+                zipFileProcessor.getPdfs(),
+                null
+            )
+        ).getCause();
+
+        assertThat(throwable).isInstanceOf(FileNameIrregularitiesException.class)
+            .hasMessage("Missing PDFs: 1111001.pdf, 1111005.pdf");
+    }
+
+    private ZipFileProcessor getZipFileProcessor(String zipFileName) throws IOException, URISyntaxException {
         ZipFileProcessor processor = new ZipFileProcessor("container", zipFileName);
 
         try (ZipInputStream zis = getZipInputStream(zipFileName)) {
@@ -120,22 +88,12 @@ public class ZipFileProcessorValidationTest {
             processor.setEnvelope(EntityParser.parseEnvelopeMetadata(
                 new ByteArrayInputStream(processor.getMetadata())
             ));
-        } catch (NullPointerException | UnrecognizedPropertyException exception) {
-            // nothing to see here:
-            // - some zip files does not have metadata
-            // - we are forcing json metafile to be invalid
-        } catch (NonPdfFileFoundException exception) {
-            //Zip '5_24-06-2018-00-00-00.zip' contains non-pdf file: cheque.gif
-            assertThat(exception.getMessage()).containsPattern("Zip '"
-                + zipFileName + "' contains non-pdf file: "
-                + ".+\\.(jpg|gif)"
-            );
         }
 
         return processor;
     }
 
-    private ZipInputStream getZipInputStream(String fileName) throws IOException, URISyntaxException {
-        return new ZipInputStream(Files.newInputStream(Paths.get(getResource(fileName).toURI())));
+    private ZipInputStream getZipInputStream(String zipFileName) throws IOException, URISyntaxException {
+        return new ZipInputStream(Files.newInputStream(Paths.get(getResource(zipFileName).toURI())));
     }
 }
