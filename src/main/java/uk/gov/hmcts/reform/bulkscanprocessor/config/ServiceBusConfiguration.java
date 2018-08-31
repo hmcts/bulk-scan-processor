b613abd7c9bc3c7a7652774cda315ef8422f47dd
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-08-31 09:35:31 +0100

    Envelope queue send default connection string

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ServiceBusConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ServiceBusConfiguration.java
@@ -5,6 +5,8 @@ import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServiceBusConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(ServiceBusConfiguration.class);
+
     @Bean
     public QueueClient getQueueClient(
         @Value("${servicebus.queue_envelope_send}") String connectionString
     ) throws ServiceBusException, InterruptedException {
+
+        log.info("Queue Client connection string = [{}]", connectionString);
 
         return new QueueClient(
             new ConnectionStringBuilder(connectionString),
