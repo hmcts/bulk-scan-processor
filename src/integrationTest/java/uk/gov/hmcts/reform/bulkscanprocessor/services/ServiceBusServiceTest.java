6e884719d387bb56d7e7e65ccf0acca31ebb9b46
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-08-31 16:49:30 +0100

    Move client creation/connection to supplier

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/services/ServiceBusServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/services/ServiceBusServiceTest.java
@@ -1,36 +1,50 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.services;
 
 import com.microsoft.azure.servicebus.IMessage;
-import com.microsoft.azure.servicebus.QueueClient;
+import com.microsoft.azure.servicebus.IQueueClient;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.EnvelopeMsg;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.Msg;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.QueueClientSupplier;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class ServiceBusServiceTest {
 
     @Mock
-    QueueClient queueClient;
+    IQueueClient queueClient;
 
-    @Autowired
+    @Mock
+    QueueClientSupplier queueClientSupplier;
+
     ServiceBusService serviceBusService;
+
+    @Before
+    public void setUp() {
+        when(queueClientSupplier.get()).thenReturn(this.queueClient);
+        serviceBusService = new ServiceBusService(queueClientSupplier);
+    }
 
     @Test
     public void should_send_message_with_messageId() throws Exception {
         Msg msg = new EnvelopeMsg("envelopeId");
         serviceBusService.sendMessageAsync(msg);
-
+        
         ArgumentCaptor<IMessage> argument = ArgumentCaptor.forClass(IMessage.class);
         verify(queueClient).sendAsync(argument.capture());
-        assertThat(argument.getValue()).extracting(IMessage::getMessageId).isEqualTo(msg.getMsgId());
+        assertThat(argument.getValue())
+            .extracting(IMessage::getMessageId).containsExactly(msg.getMsgId());
     }
 
 }
