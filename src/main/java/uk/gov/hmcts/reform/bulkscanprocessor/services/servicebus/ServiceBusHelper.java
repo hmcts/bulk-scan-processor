5fcd88b73ee700c5bfdddd0ab5da6dacb085aab9
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-09-01 21:44:34 +0100

    Make client creation/connection lazy in a different way

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/servicebus/ServiceBusHelper.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/servicebus/ServiceBusHelper.java
@@ -2,14 +2,15 @@ package uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus;
 
 import com.microsoft.azure.servicebus.IQueueClient;
 import com.microsoft.azure.servicebus.Message;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.Msg;
 
 import java.util.concurrent.CompletableFuture;
 
 @Component
-@Lazy
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class ServiceBusHelper {
 
     private final IQueueClient sendClient;
