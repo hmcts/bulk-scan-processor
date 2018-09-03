21271639a26f7772b7ecb2ce9a0afa1db995e05c
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-09-03 13:04:38 +0100

    Fix variable name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -99,7 +99,7 @@ module "bulk-scan" {
     SCAN_ENABLED                  = "${var.scan_enabled}"
     STORAGE_BLOB_LEASE_TIMEOUT    = "${var.blob_lease_timeout}" // In seconds
 
-    QUEUE_ENVELOPE_SEND           = "${data.azurerm_key_vault_secret.queue_envelope_send.value}"
+    QUEUE_ENVELOPE_SEND           = "${data.azurerm_key_vault_secret.queue_send_connection_string.value}"
 
     // silence the "bad implementation" logs
     LOGBACK_REQUIRE_ALERT_LEVEL   = false
@@ -186,7 +186,7 @@ data "vault_generic_secret" "s2s_secret_test" {
   path = "secret/${var.vault_section}/ccidam/service-auth-provider/api/microservice-keys/bulk-scan-processor-tests"
 }
 
-data "azurerm_key_vault_secret" "queue_envelope_send" {
+data "azurerm_key_vault_secret" "queue_send_connection_string" {
   name      = "envelope-queue-send-conn-string"
   vault_uri = "${module.bulk-scan-key-vault.key_vault_uri}"
 }
