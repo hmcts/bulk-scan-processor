ceb5d0eada63ad30643508c290a9351f44d3e01f
Aliveni Choppa <aliveni.ch@gmail.com>
2018-09-11 15:48:02 +0100

    RPE-701 updated sql file to specify that payment instrument number can be null

--- src/main/resources/db/migration/V013__Add_new_fields.sql
+++ src/main/resources/db/migration/V013__Add_new_fields.sql
@@ -5,7 +5,7 @@ ALTER TABLE non_scannable_items
   ADD COLUMN documentControlNumber VARCHAR(100) NOT NULL;
 
 ALTER TABLE payments
-  ADD COLUMN paymentInstrumentNumber VARCHAR(100),
+  ADD COLUMN paymentInstrumentNumber VARCHAR(100) NULL,
   ADD COLUMN sortCode VARCHAR(10) NULL,
   ADD COLUMN accountNumber VARCHAR(50) NULL;
 
