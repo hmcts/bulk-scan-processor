2436e029892720356b3d742f837f0f63edaf0e57
Aliveni Choppa <aliveni.ch@gmail.com>
2018-09-13 16:24:54 +0100

    Set default values for non nullable fields

--- src/main/resources/db/migration/V013__Add_new_fields.sql
+++ src/main/resources/db/migration/V013__Add_new_fields.sql
@@ -2,7 +2,7 @@ ALTER TABLE envelopes
   ADD COLUMN caseNumber VARCHAR(100) NULL;
 
 ALTER TABLE non_scannable_items
-  ADD COLUMN documentControlNumber VARCHAR(100) NULL;
+  ADD COLUMN documentControlNumber VARCHAR(100) NOT NULL DEFAULT "1111111" ;
 
 ALTER TABLE payments
   ADD COLUMN paymentInstrumentNumber VARCHAR(100) NULL,
@@ -10,6 +10,6 @@ ALTER TABLE payments
   ADD COLUMN accountNumber VARCHAR(50) NULL;
 
 UPDATE payments
-  SET sortCode = '112233',
-      accountNumber = '12345678'
+  SET sortCode = '111111',
+      accountNumber = '11111111'
   WHERE method = 'Cheque' AND sortCode IS NULL AND accountNumber IS NULL;
