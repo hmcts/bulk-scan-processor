6572b289684280ee45f727e4c6171a5d682f2d23
Aliveni Choppa <aliveni.ch@gmail.com>
2018-09-13 16:07:39 +0100

    Fixed dbMigrate schema

--- src/main/resources/db/migration/V013__Add_new_fields.sql
+++ src/main/resources/db/migration/V013__Add_new_fields.sql
@@ -2,15 +2,12 @@ ALTER TABLE envelopes
   ADD COLUMN caseNumber VARCHAR(100) NULL;
 
 ALTER TABLE non_scannable_items
-  ADD COLUMN documentControlNumber VARCHAR(100) NOT NULL;
+  ADD COLUMN documentControlNumber VARCHAR(100) NULL;
 
 ALTER TABLE payments
   ADD COLUMN paymentInstrumentNumber VARCHAR(100) NULL,
   ADD COLUMN sortCode VARCHAR(10) NULL,
   ADD COLUMN accountNumber VARCHAR(50) NULL;
-
-ALTER TABLE scannable_items
-  ALTER COLUMN documentControlNumber SET NOT NULL;
 
 UPDATE payments
   SET sortCode = '112233',
