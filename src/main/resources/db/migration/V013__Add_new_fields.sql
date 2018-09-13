f02c9a26b68752299330c802c211bb6e31a1a7f7
aliveni <aliveni.ch@gmail.com>
2018-09-13 20:24:06 +0100

    [PREVIEW] Fix/set default document control number for nonscannable items (#169)
    * Fixed dbMigrate schema
    
    * Set default values for non nullable fields
    
    * Replaced double quotes with single quote

--- src/main/resources/db/migration/V013__Add_new_fields.sql
+++ src/main/resources/db/migration/V013__Add_new_fields.sql
@@ -2,17 +2,14 @@ ALTER TABLE envelopes
   ADD COLUMN caseNumber VARCHAR(100) NULL;
 
 ALTER TABLE non_scannable_items
-  ADD COLUMN documentControlNumber VARCHAR(100) NOT NULL;
+  ADD COLUMN documentControlNumber VARCHAR(100) NOT NULL DEFAULT '1111111';
 
 ALTER TABLE payments
   ADD COLUMN paymentInstrumentNumber VARCHAR(100) NULL,
   ADD COLUMN sortCode VARCHAR(10) NULL,
   ADD COLUMN accountNumber VARCHAR(50) NULL;
 
-ALTER TABLE scannable_items
-  ALTER COLUMN documentControlNumber SET NOT NULL;
-
 UPDATE payments
-  SET sortCode = '112233',
-      accountNumber = '12345678'
+  SET sortCode = '111111',
+      accountNumber = '11111111'
   WHERE method = 'Cheque' AND sortCode IS NULL AND accountNumber IS NULL;
