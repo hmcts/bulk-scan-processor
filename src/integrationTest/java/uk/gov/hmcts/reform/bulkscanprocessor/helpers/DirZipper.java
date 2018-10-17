4e7aee32fa3a0adb16fbd44088ab392956d80a26
Rafal Kondratowicz <rkondr@gmail.com>
2018-10-17 11:31:19 +0100

    cleanup

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/helpers/DirZipper.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/helpers/DirZipper.java
@@ -1,34 +1,29 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.helpers;
 
 import com.google.common.io.Files;
-import org.apache.commons.io.FileUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import static com.google.common.io.Resources.getResource;
-import static com.google.common.io.Resources.toByteArray;
 import static java.util.stream.Collectors.toList;
 
 public class DirZipper {
 
     /**
-     * Zips files from given directory. Files in resulting archive are NOT 'wrapped' in a directory.
+     * Zips files from given directory. Files in resulting archive are NOT wrapped in a directory.
      */
     public static byte[] zipDir(String dirName) throws IOException {
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try (ZipOutputStream zos = new ZipOutputStream(outputStream)) {
 
-            // .list() does not guarantee any order.
+            // .listFiles() does not guarantee any order.
             // Files need to be sorted as currently mocks rely on files order.
             // TODO: improve mocks in tests so that order does not matter
             List<File> filesToZip =
@@ -38,7 +33,6 @@ public class DirZipper {
 
             for (File file : filesToZip) {
                 zos.putNextEntry(new ZipEntry(file.getName()));
-                //zos.write(toByteArray(getResource(file)));
                 zos.write(Files.toByteArray(file));
                 zos.closeEntry();
             }
