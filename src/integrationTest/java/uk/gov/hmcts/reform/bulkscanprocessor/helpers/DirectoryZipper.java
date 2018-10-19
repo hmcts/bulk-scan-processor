cfc4225c55218b3e2484ba74004680e872abff41
Rafal Kondratowicz <rkondr@gmail.com>
2018-10-19 14:27:00 +0100

    Remove zip files from tests (part 8)

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/helpers/DirectoryZipper.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/helpers/DirectoryZipper.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.helpers;
 
 import com.google.common.io.Files;
+import uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor.ZipVerifiers;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -11,7 +12,10 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import static com.google.common.io.Resources.getResource;
+import static com.google.common.io.Resources.toByteArray;
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
+import static uk.gov.hmcts.reform.bulkscanprocessor.helpers.SigningHelper.signWithSha256Rsa;
 
 public final class DirectoryZipper {
 
@@ -20,25 +24,64 @@ public final class DirectoryZipper {
      */
     public static byte[] zipDir(String dirName) throws IOException {
 
+        // .listFiles() does not guarantee any order.
+        // Files need to be sorted as currently mocks rely on files order.
+        // TODO: improve mocks in tests so that order does not matter
+        return zipItems(
+            Stream.of(new File(getResource(dirName).getPath()).listFiles())
+                .sorted()
+                .map(f -> new ZipItem(f.getName(), getFileBytes(f)))
+                .collect(toList())
+        );
+    }
+
+    /**
+     * Zips files from given directory, generates a signature for the resulting archive and then zips both file.
+     */
+    public static byte[] zipAndSignDir(String dirName, String signingKeyName) throws Exception {
+
+        byte[] innerZip = zipDir(dirName);
+        byte[] signature = signWithSha256Rsa(innerZip, toByteArray(getResource(signingKeyName)));
+
+        return zipItems(
+            asList(
+                new ZipItem(ZipVerifiers.DOCUMENTS_ZIP, innerZip),
+                new ZipItem(ZipVerifiers.SIGNATURE_SIG, signature)
+            )
+        );
+    }
+
+    private static byte[] zipItems(List<ZipItem> items) throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try (ZipOutputStream zos = new ZipOutputStream(outputStream)) {
 
-            // .listFiles() does not guarantee any order.
-            // Files need to be sorted as currently mocks rely on files order.
-            // TODO: improve mocks in tests so that order does not matter
-            List<File> filesToZip =
-                Stream.of(new File(getResource(dirName).getPath()).listFiles())
-                    .sorted()
-                    .collect(toList());
-
-            for (File file : filesToZip) {
-                zos.putNextEntry(new ZipEntry(file.getName()));
-                zos.write(Files.toByteArray(file));
+            for (ZipItem item : items) {
+                zos.putNextEntry(new ZipEntry(item.name));
+                zos.write(item.content);
                 zos.closeEntry();
             }
         }
 
         return outputStream.toByteArray();
+    }
+
+    private static byte[] getFileBytes(File file) {
+        // wrap in runtime exception so that this can be used in .map()
+        try {
+            return Files.toByteArray(file);
+        } catch (IOException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+    private static class ZipItem {
+        final String name;
+        final byte[] content;
+
+        ZipItem(String name, byte[] content) {
+            this.name = name;
+            this.content = content;
+        }
     }
 
     private DirectoryZipper() {
