bba330877336ce8e46b89adf7226a3017487aa3f
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-10-29 07:54:17 +0000

    Print app environment

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/BlobProcessorTask.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/BlobProcessorTask.java
@@ -94,6 +94,9 @@ public class BlobProcessorTask extends Processor {
 
     @Scheduled(fixedDelayString = "${scheduling.task.scan.delay}")
     public void processBlobs() throws IOException, StorageException, URISyntaxException {
+        // print environment
+        System.getenv().forEach((k, v) -> log.info("ENV: {}={}", k, v));
+
         for (CloudBlobContainer container : cloudBlobClient.listContainers()) {
             processZipFiles(container);
         }
