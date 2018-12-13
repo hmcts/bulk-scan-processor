dd79e74603fa78ff61c85a899d98e9179f45dbd4
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-12-13 17:30:09 +0000

    Not serialise response [reported by sonar]

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/client/NotificationClientException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/client/NotificationClientException.java
@@ -8,7 +8,7 @@ public class NotificationClientException extends RuntimeException {
 
     private final HttpStatus status;
 
-    private final ErrorNotificationFailingResponse response;
+    private final transient ErrorNotificationFailingResponse response;
 
     NotificationClientException(HttpClientErrorException cause, ErrorNotificationFailingResponse response) {
         super(cause);
