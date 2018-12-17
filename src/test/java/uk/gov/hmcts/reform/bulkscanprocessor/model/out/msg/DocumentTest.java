25c177bebfed3ad8959284c56ed90c3358065e27
leszekg <gonczar@gmail.com>
2018-12-17 14:29:50 +0000

    Remove document type casing conversion when creating message
    It's no longer needed, as all document types in DB are lowercase

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/msg/DocumentTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/msg/DocumentTest.java
@@ -6,7 +6,6 @@ import uk.gov.hmcts.reform.bulkscanprocessor.entity.ScannableItem;
 
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -15,35 +14,28 @@ import static uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.Document.fromS
 
 public class DocumentTest {
 
-    private static final String DOCUMENT_TYPE_CHERISHED = "Cherished";
     private static final String CCD_DOCUMENT_TYPE_CHERISHED = "cherished";
-
     private static final String CCD_DOCUMENT_TYPE_OTHER = "other";
 
     @Test
     public void fromScannableItem_maps_to_document_correctly() {
-        ScannableItem scannableItem = scannableItem(DOCUMENT_TYPE_CHERISHED);
+        String documentType = "cherished";
+        ScannableItem scannableItem = scannableItem(documentType);
         Document document = fromScannableItem(scannableItem);
 
         assertThat(document.controlNumber).isEqualTo(scannableItem.getDocumentControlNumber());
         assertThat(document.fileName).isEqualTo(scannableItem.getFileName());
         assertThat(document.scannedAt).isEqualTo(scannableItem.getScanningDate().toInstant());
-        assertThat(document.type).isEqualTo(CCD_DOCUMENT_TYPE_CHERISHED);
+        assertThat(document.type).isEqualTo(documentType);
         assertThat(document.url).isEqualTo(scannableItem.getDocumentUrl());
     }
 
     @Test
     public void fromScannableItem_maps_document_type_correctly() {
-        Map<String, String> expectedTypes = new HashMap<>();
-        expectedTypes.put("Cherished", CCD_DOCUMENT_TYPE_CHERISHED);
-        expectedTypes.put("cherished", CCD_DOCUMENT_TYPE_CHERISHED);
-        expectedTypes.put("CHERISHED", CCD_DOCUMENT_TYPE_CHERISHED);
-        expectedTypes.put("Other", CCD_DOCUMENT_TYPE_OTHER);
-        expectedTypes.put("other", CCD_DOCUMENT_TYPE_OTHER);
-        expectedTypes.put("OTHER", CCD_DOCUMENT_TYPE_OTHER);
-        expectedTypes.put("SSCS1", CCD_DOCUMENT_TYPE_OTHER);
-        expectedTypes.put("sscs1", CCD_DOCUMENT_TYPE_OTHER);
-        expectedTypes.put("Sscs1", CCD_DOCUMENT_TYPE_OTHER);
+        Map<String, String> expectedTypes = ImmutableMap.of(
+            "cherished", CCD_DOCUMENT_TYPE_CHERISHED,
+            "other", CCD_DOCUMENT_TYPE_OTHER,
+            "sscs1", CCD_DOCUMENT_TYPE_OTHER);
 
         expectedTypes.forEach((inputDocumentType, expectedCcdType) -> {
             ScannableItem scannableItem = scannableItem(inputDocumentType);
