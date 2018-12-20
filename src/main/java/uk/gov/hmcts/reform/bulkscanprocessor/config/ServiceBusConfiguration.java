7e23e117cb5db7f26b4714826ce4a0cc5a5ca428
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-12-20 09:52:06 +0000

    Introduce message handler

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ServiceBusConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ServiceBusConfiguration.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.servicebus.IMessageHandler;
+import com.microsoft.azure.servicebus.IQueueClient;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
@@ -9,11 +11,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.ErrorNotificationService;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusHelper;
+import uk.gov.hmcts.reform.bulkscanprocessor.tasks.ErrorNotificationHandler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Lazy
 @Configuration
 public class ServiceBusConfiguration {
+
+    static final ExecutorService QUEUE_READ_EXEC = Executors.newSingleThreadExecutor(r ->
+        new Thread(r, "notifications-queue-read")
+    );
 
     @Bean(name = "envelopes")
     public ServiceBusHelper envelopesQueueHelper(
@@ -43,5 +54,24 @@ public class ServiceBusConfiguration {
             ),
             objectMapper
         );
+    }
+
+    @Bean(name = "notifications-read")
+    public IQueueClient notificationsQueueReader(
+        @Value("${queues.read-notifications.connection-string}") String connectionString,
+        @Value("${queues.read-notifications.queue-name}") String queueName,
+        ErrorNotificationService service,
+        ObjectMapper objectMapper
+    ) throws InterruptedException, ServiceBusException {
+        IQueueClient client = new QueueClient(
+            new ConnectionStringBuilder(connectionString, queueName),
+            ReceiveMode.PEEKLOCK
+        );
+
+        IMessageHandler messageHandler = new ErrorNotificationHandler(service, objectMapper);
+
+        client.registerMessageHandler(messageHandler, QUEUE_READ_EXEC);
+
+        return client;
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ErrorNotificationHandler.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.servicebus.ExceptionPhase;
+import com.microsoft.azure.servicebus.IMessage;
+import com.microsoft.azure.servicebus.IMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.InvalidMessageException;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.ErrorMsg;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.ErrorNotificationService;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ErrorNotificationHandler implements IMessageHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ErrorNotificationHandler.class);
+
+    private final ErrorNotificationService service;
+
+    private final ObjectMapper mapper;
+
+    static final Executor SIMPLE_EXEC = Runnable::run;
+
+    static final Executor SERVICE_EXEC = Executors.newSingleThreadExecutor(r ->
+        new Thread(r, "error-notification-service")
+    );
+
+
+    public ErrorNotificationHandler(
+        ErrorNotificationService service,
+        ObjectMapper mapper
+    ) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public CompletableFuture<Void> onMessageAsync(IMessage message) {
+        return CompletableFuture
+            .supplyAsync(message::getBody, SIMPLE_EXEC)
+            .thenApplyAsync(this::getErrorMessage, SIMPLE_EXEC)
+            .thenAcceptAsync(this::processMessage, SERVICE_EXEC)
+            .thenRunAsync(() -> log.debug("Error notification consumed. ID {}", message.getMessageId()), SIMPLE_EXEC);
+    }
+
+    private ErrorMsg getErrorMessage(byte[] message) {
+        try {
+            return mapper.readValue(message, ErrorMsg.class);
+        } catch (IOException exception) {
+            throw new InvalidMessageException(
+                "Unable to read error message. Message: " + exception.getMessage(),
+                exception
+            );
+        }
+    }
+
+    private void processMessage(ErrorMsg message) {
+        service.processServiceBusMessage(message);
+    }
+
+    @Override
+    public void notifyException(Throwable exception, ExceptionPhase phase) {
+        log.error(
+            "Exception occurred in phase {}. Message: {}",
+            phase,
+            exception.getMessage(),
+            exception
+        );
+    }
+}
