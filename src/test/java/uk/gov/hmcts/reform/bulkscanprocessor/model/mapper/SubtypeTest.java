9e2ef757b229d04780d1dbb570b7f4e8a5cc28af
Rafal Kondratowicz <rkondr@gmail.com>
2019-01-08 13:38:02 +0000

    Test all cases for document subtypes

---
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/model/mapper/SubtypeTest.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.model.mapper;
+
+import org.junit.Test;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.ScannableItem;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputDocumentType;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputScannableItem;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.common.DocumentSubtype;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.common.DocumentType;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.bulkscanprocessor.helper.EnvelopeCreator.inputScannableItem;
+
+public class SubtypeTest {
+
+    @Test
+    public void should_map_scannable_item_document_types_correctly() {
+        asList(
+            new TestCase(inputScannableItem(InputDocumentType.SSCS1), DocumentType.OTHER, DocumentSubtype.SSCS1),
+            new TestCase(inputScannableItem(InputDocumentType.WILL), DocumentType.OTHER, DocumentSubtype.WILL),
+            new TestCase(inputScannableItem(InputDocumentType.CHERISHED), DocumentType.CHERISHED, null),
+            new TestCase(inputScannableItem(InputDocumentType.OTHER), DocumentType.OTHER, null)
+        ).forEach(testCase -> {
+            // given
+            InputScannableItem item = testCase.input;
+
+            // when
+            ScannableItem result = EnvelopeMapper.toDbScannableItem(item);
+
+            // then
+            assertThat(result.getDocumentType()).isEqualTo(testCase.expectedDocType);
+            assertThat(result.getDocumentSubtype()).isEqualTo(testCase.expectedDocSubtype);
+        });
+    }
+
+    private class TestCase {
+        InputScannableItem input;
+        DocumentType expectedDocType;
+        DocumentSubtype expectedDocSubtype;
+
+        // region constructor
+        TestCase(InputScannableItem input, DocumentType expectedDocType, DocumentSubtype expectedDocSubtype) {
+            this.input = input;
+            this.expectedDocType = expectedDocType;
+            this.expectedDocSubtype = expectedDocSubtype;
+        }
+        // endregion
+    }
+}
