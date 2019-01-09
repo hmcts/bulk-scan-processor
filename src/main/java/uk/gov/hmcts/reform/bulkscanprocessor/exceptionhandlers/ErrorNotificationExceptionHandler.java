478ae25fbe8d47d7d65c139df0a2875a6571607e
Donatas Martinkus <donatas.martinkus@hmcts.net>
2019-01-09 12:50:12 +0000

    Replace usage of wrapper with custom auto completor

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptionhandlers/ErrorNotificationExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptionhandlers/ErrorNotificationExceptionHandler.java
@@ -2,37 +2,49 @@ package uk.gov.hmcts.reform.bulkscanprocessor.exceptionhandlers;
 
 import com.microsoft.azure.servicebus.IMessage;
 import uk.gov.hmcts.reform.bulkscanprocessor.client.ErrorNotificationException;
-import uk.gov.hmcts.reform.bulkscanprocessor.model.ErrorNotificationMessageWrapper;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.MessageAutoCompletor;
+
+import java.util.concurrent.CompletableFuture;
+
+import static uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.MessageAutoCompletor.DeadLetterReason;
 
 public class ErrorNotificationExceptionHandler {
 
-    public ErrorNotificationExceptionHandler() {
-        // empty construct
+    private final MessageAutoCompletor autoCompletor;
+
+    public ErrorNotificationExceptionHandler(MessageAutoCompletor autoCompletor) {
+        this.autoCompletor = autoCompletor;
     }
 
-    public ErrorNotificationMessageWrapper handle(IMessage message, Throwable throwable) {
+    public CompletableFuture<Void> handle(IMessage message, Throwable throwable) {
         // return for message session completion step
         return throwable != null
             ? handleNonNullThrowable(message, throwable)
-            : ErrorNotificationMessageWrapper.forAcknowledgement(message);
+            : autoCompletor.completeAsync(message.getLockToken());
     }
 
-    private ErrorNotificationMessageWrapper handleNonNullThrowable(IMessage message, Throwable throwable) {
+    private CompletableFuture<Void> handleNonNullThrowable(IMessage message, Throwable throwable) {
         if (throwable instanceof ErrorNotificationException) {
             return handleErrorNotificationException(message, (ErrorNotificationException) throwable);
         } else {
-            return ErrorNotificationMessageWrapper.forDeadLettering(message);
+            return autoCompletor.deadLetterAsync(
+                message.getLockToken(),
+                new DeadLetterReason("Unknown exception", throwable.getMessage())
+            );
         }
     }
 
-    private ErrorNotificationMessageWrapper handleErrorNotificationException(
+    private CompletableFuture<Void> handleErrorNotificationException(
         IMessage message,
         ErrorNotificationException exception
     ) {
         if (exception.getStatus().is5xxServerError()) {
-            return ErrorNotificationMessageWrapper.forAbandoning(message);
+            return autoCompletor.abandonAsync(message.getLockToken());
         } else {
-            return ErrorNotificationMessageWrapper.forDeadLettering(message);
+            return autoCompletor.deadLetterAsync(
+                message.getLockToken(),
+                new DeadLetterReason("Client error", exception.getMessage())
+            );
         }
     }
 }
--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptionhandlers/ErrorNotificationExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptionhandlers/ErrorNotificationExceptionHandlerTest.java
@@ -1,60 +1,88 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.exceptionhandlers;
 
 import com.microsoft.azure.servicebus.IMessage;
+import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.Message;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpStatusCodeException;
 import uk.gov.hmcts.reform.bulkscanprocessor.client.ErrorNotificationException;
-import uk.gov.hmcts.reform.bulkscanprocessor.model.ErrorNotificationMessageWrapper;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.MessageAutoCompletor;
 
 import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.spy;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ErrorNotificationExceptionHandlerTest {
 
-    private static final IMessage MESSAGE = new Message("content");
+    @Mock
+    private IMessageReceiver receiver;
 
-    private static final ErrorNotificationExceptionHandler HANDLER = new ErrorNotificationExceptionHandler();
+    private ErrorNotificationExceptionHandler handler;
+
+    private static final IMessage MESSAGE = spy(new Message("content"));
+
+    private static final CompletableFuture<Void> COMPLETED_FUTURE = CompletableFuture.completedFuture(null);
+
+    @Before
+    public void setUp() {
+        handler = new ErrorNotificationExceptionHandler(new MessageAutoCompletor(receiver));
+
+        given(MESSAGE.getLockToken()).willReturn(UUID.randomUUID());
+    }
 
     @Test
     public void should_mark_for_acknowledgement_when_no_exception_sent_to_handle() {
-        ErrorNotificationMessageWrapper wrapper = HANDLER.handle(MESSAGE, null);
+        given(receiver.completeAsync(any(UUID.class))).willReturn(COMPLETED_FUTURE);
 
-        assertThat(wrapper.isAssignedForDeadLettering()).isFalse();
-        assertThat(wrapper.isCompletedAcknowledgement()).isTrue();
+        CompletableFuture<Void> handled = handler.handle(MESSAGE, null);
+
+        assertThat(handled.join()).isNull();
     }
 
     @Test
     public void should_mark_for_deadletter_when_exception_is_not_ErrorNotificationException() {
-        ErrorNotificationMessageWrapper wrapper = HANDLER.handle(MESSAGE, new IOException("oh no"));
+        given(receiver.deadLetterAsync(any(UUID.class), anyString(), anyString())).willReturn(COMPLETED_FUTURE);
 
-        assertThat(wrapper.isAssignedForDeadLettering()).isTrue();
-        assertThat(wrapper.isCompletedAcknowledgement()).isFalse();
+        CompletableFuture<Void> handled = handler.handle(MESSAGE, new IOException("oh no"));
+
+        assertThat(handled.join()).isNull();
     }
 
     @Test
     public void should_mark_for_deadletter_when_exception_is_4xx_of_notification_exception() {
+        given(receiver.deadLetterAsync(any(UUID.class), anyString(), anyString())).willReturn(COMPLETED_FUTURE);
+
         ErrorNotificationException exception = new ErrorNotificationException(
             new HttpStatusCodeException(HttpStatus.BAD_REQUEST) {},
             null
         );
-        ErrorNotificationMessageWrapper wrapper = HANDLER.handle(MESSAGE, exception);
+        CompletableFuture<Void> handled = handler.handle(MESSAGE, exception);
 
-        assertThat(wrapper.isAssignedForDeadLettering()).isTrue();
-        assertThat(wrapper.isCompletedAcknowledgement()).isFalse();
+        assertThat(handled.join()).isNull();
     }
 
     @Test
     public void should_mark_for_abandonment_when_exception_is_5xx_of_notification_exception() {
+        given(receiver.abandonAsync(any(UUID.class), any())).willReturn(COMPLETED_FUTURE);
+
         ErrorNotificationException exception = new ErrorNotificationException(
             new HttpStatusCodeException(HttpStatus.INTERNAL_SERVER_ERROR) {},
             null
         );
-        ErrorNotificationMessageWrapper wrapper = HANDLER.handle(MESSAGE, exception);
+        CompletableFuture<Void> handled = handler.handle(MESSAGE, exception);
 
-        assertThat(wrapper.isAssignedForDeadLettering()).isFalse();
-        assertThat(wrapper.isCompletedAcknowledgement()).isFalse();
+        assertThat(handled.join()).isNull();
     }
 }
