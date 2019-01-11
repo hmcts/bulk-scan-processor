37f1c633286e6a2eb44f7e4e3abe21e0c6d126d0
Donatas Martinkus <donatas.martinkus@hmcts.net>
2019-01-11 11:56:25 +0000

    Configure queue read client with custom message handler

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ServiceBusConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ServiceBusConfiguration.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.servicebus.IMessageHandler;
+import com.microsoft.azure.servicebus.IQueueClient;
+import com.microsoft.azure.servicebus.MessageHandlerOptions;
 import com.microsoft.azure.servicebus.QueueClient;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
@@ -9,11 +12,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.ErrorNotificationService;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.MessageAutoCompletor;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusHelper;
+import uk.gov.hmcts.reform.bulkscanprocessor.tasks.ErrorNotificationHandler;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Lazy
 @Configuration
 public class ServiceBusConfiguration {
+
+    private static final ExecutorService QUEUE_READ_EXEC = Executors.newSingleThreadExecutor(r ->
+        new Thread(r, "notifications-queue-read")
+    );
 
     @Bean(name = "envelopes")
     public ServiceBusHelper envelopesQueueHelper(
@@ -43,5 +57,32 @@ public class ServiceBusConfiguration {
             ),
             objectMapper
         );
+    }
+
+    @Bean(name = "notifications-read")
+    public IQueueClient notificationsQueueReader(
+        @Value("${queues.read-notifications.connection-string}") String connectionString,
+        @Value("${queues.read-notifications.queue-name}") String queueName,
+        ErrorNotificationService service,
+        ObjectMapper objectMapper
+    ) throws InterruptedException, ServiceBusException {
+        IQueueClient client = new QueueClient(
+            new ConnectionStringBuilder(connectionString, queueName),
+            ReceiveMode.PEEKLOCK
+        );
+
+        IMessageHandler messageHandler = new ErrorNotificationHandler(
+            service,
+            objectMapper,
+            new MessageAutoCompletor(client)
+        );
+
+        client.registerMessageHandler(
+            messageHandler,
+            new MessageHandlerOptions(1, false, Duration.ofMinutes(5)),
+            QUEUE_READ_EXEC
+        );
+
+        return client;
     }
 }
