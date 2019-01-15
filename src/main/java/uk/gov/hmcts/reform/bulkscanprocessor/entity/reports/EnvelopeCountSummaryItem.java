f0b2d9dc51dca5a4a085d330aa574fe8db815b6d
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-01-15 16:20:40 +0000

    Add repository layer for envelope count summary report (#422)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/reports/EnvelopeCountSummaryItem.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.entity.reports;
+
+import java.time.LocalDate;
+
+public interface EnvelopeCountSummaryItem {
+
+    LocalDate getDate();
+
+    String getJurisdiction();
+
+    int getReceived();
+
+    int getRejected();
+}
