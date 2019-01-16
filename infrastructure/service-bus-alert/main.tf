44093e464a85a27d55ee6f065e3a746b14bd3ac2
Donatas Martinkus <donatas.martinkus@hmcts.net>
2019-01-16 12:56:15 +0000

    Scrap some alert based on schema. Really scrap

---
+++ infrastructure/service-bus-alert/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  version = "1.19.0"
+}
+
+data "template_file" "metricalerttemplate" {
+  template = "${file("${path.module}/templates/ServiceBusAlert.json")}"
+}
+
+resource "azurerm_template_deployment" "custom_alert" {
+  template_body       = "${data.template_file.metricalerttemplate.rendered}"
+  name                = "NAME?"
+  resourceGroupName = "${var.resource_group_name}"
+  deployment_mode     = "Incremental"
+
+  parameters = {
+  }
+}
