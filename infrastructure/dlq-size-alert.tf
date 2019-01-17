ae686014c30fdf127d07960aaa92c80faa98470b
Donatas Martinkus <donatas.martinkus@hmcts.net>
2019-01-17 09:35:01 +0000

    Fix module parameters

--- infrastructure/dlq-size-alert.tf
+++ infrastructure/dlq-size-alert.tf
@@ -1,6 +1,6 @@
 module "dlq-size-alert" {
   source            = "./service-bus-alert"
   subscription_id = "${var.subscription}"
-  resource_group_name = "${var.product}-${var.component}-${var.env}"
+  resourcegroup_name = "${var.product}-${var.env}"
   namespace = "${var.deployment_namespace}"
 }
