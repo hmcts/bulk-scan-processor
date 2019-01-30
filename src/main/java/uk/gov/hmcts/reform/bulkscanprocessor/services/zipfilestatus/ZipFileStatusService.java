356acb574cbd694662cea804d1833876245bc906
Rafal Kondratowicz <rkondr@gmail.com>
2019-01-30 16:27:48 +0000

    Move mapping of zip file status models to service

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/zipfilestatus/ZipFileStatusService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/zipfilestatus/ZipFileStatusService.java
@@ -3,7 +3,11 @@ package uk.gov.hmcts.reform.bulkscanprocessor.services.zipfilestatus;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEventRepository;
-import uk.gov.hmcts.reform.bulkscanprocessor.services.zipfilestatus.model.ZipFileStatus;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileEnvelope;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileEvent;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileStatus;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class ZipFileStatusService {
@@ -22,8 +26,29 @@ public class ZipFileStatusService {
 
     public ZipFileStatus getStatusFor(String zipFileName) {
         return new ZipFileStatus(
-            envelopeRepo.findByZipFileName(zipFileName),
-            eventRepo.findByZipFileName(zipFileName)
+            envelopeRepo
+                .findByZipFileName(zipFileName)
+                .stream()
+                .map(envelope ->
+                    new ZipFileEnvelope(
+                        envelope.getId().toString(),
+                        envelope.getContainer(),
+                        envelope.getStatus().name()
+                    )
+                )
+                .collect(toList()),
+            eventRepo
+                .findByZipFileName(zipFileName)
+                .stream()
+                .map(event ->
+                    new ZipFileEvent(
+                        event.getEvent().name(),
+                        event.getContainer(),
+                        event.getCreatedAt()
+                    )
+                )
+                .collect(toList())
+
         );
     }
 }
--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/zipfilestatus/ZipFileStatusServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/zipfilestatus/ZipFileStatusServiceTest.java
@@ -10,16 +10,21 @@ import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEvent;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEventRepository;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.Status;
-import uk.gov.hmcts.reform.bulkscanprocessor.helper.EnvelopeCreator;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.common.Event;
-import uk.gov.hmcts.reform.bulkscanprocessor.services.zipfilestatus.model.ZipFileStatus;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileEnvelope;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileEvent;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileStatus;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.UUID;
 
+import static java.time.Instant.now;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ZipFileStatusServiceTest {
@@ -37,17 +42,15 @@ public class ZipFileStatusServiceTest {
     @Test
     public void should_return_envelopes_and_events_from_db() {
         // given
-        List<ProcessEvent> events = asList(
-            new ProcessEvent("container_A", "hello.zip", Event.DOC_UPLOADED),
-            new ProcessEvent("container_A", "hello.zip", Event.DOC_PROCESSED),
-            new ProcessEvent("container_B", "hello.zip", Event.DOC_UPLOADED),
-            new ProcessEvent("container_B", "hello.zip", Event.DOC_PROCESSED),
-            new ProcessEvent("container_C", "hello.zip", Event.DOC_FAILURE)
+        List<Envelope> envelopes = asList(
+            envelope(UUID.randomUUID(), "A", Status.PROCESSED),
+            envelope(UUID.randomUUID(), "B", Status.PROCESSED)
         );
 
-        List<Envelope> envelopes = asList(
-            EnvelopeCreator.envelope("container_A", Status.PROCESSED),
-            EnvelopeCreator.envelope("container_B", Status.PROCESSED)
+        List<ProcessEvent> events = asList(
+            event(Event.DOC_UPLOADED, "A", Timestamp.from(now())),
+            event(Event.DOC_PROCESSED, "A", Timestamp.from(now().minusSeconds(1))),
+            event(Event.DOC_FAILURE, "B", Timestamp.from(now().minusSeconds(2)))
         );
 
         given(envelopeRepo.findByZipFileName("hello.zip")).willReturn(envelopes);
@@ -59,11 +62,38 @@ public class ZipFileStatusServiceTest {
         // then
         assertThat(result.envelopes)
             .usingFieldByFieldElementComparator()
-            .containsExactlyInAnyOrderElementsOf(envelopes);
+            .containsExactlyInAnyOrder(
+                new ZipFileEnvelope(
+                    envelopes.get(0).getId().toString(),
+                    envelopes.get(0).getContainer(),
+                    envelopes.get(0).getStatus().name()
+                ),
+                new ZipFileEnvelope(
+                    envelopes.get(1).getId().toString(),
+                    envelopes.get(1).getContainer(),
+                    envelopes.get(1).getStatus().name()
+                )
+            );
 
         assertThat(result.events)
             .usingFieldByFieldElementComparator()
-            .containsExactlyInAnyOrderElementsOf(events);
+            .containsExactlyInAnyOrder(
+                new ZipFileEvent(
+                    events.get(0).getEvent().name(),
+                    events.get(0).getContainer(),
+                    events.get(0).getCreatedAt()
+                ),
+                new ZipFileEvent(
+                    events.get(1).getEvent().name(),
+                    events.get(1).getContainer(),
+                    events.get(1).getCreatedAt()
+                ),
+                new ZipFileEvent(
+                    events.get(2).getEvent().name(),
+                    events.get(2).getContainer(),
+                    events.get(2).getCreatedAt()
+                )
+            );
     }
 
     @Test
@@ -79,5 +109,21 @@ public class ZipFileStatusServiceTest {
         assertThat(result).isNotNull();
         assertThat(result.envelopes).isNotNull().isEmpty();
         assertThat(result.events).isNotNull().isEmpty();
+    }
+
+    private Envelope envelope(UUID id, String container, Status status) {
+        Envelope envelope = mock(Envelope.class);
+        given(envelope.getId()).willReturn(id);
+        given(envelope.getContainer()).willReturn(container);
+        given(envelope.getStatus()).willReturn(status);
+        return envelope;
+    }
+
+    private ProcessEvent event(Event eventType, String container, Timestamp createdAt) {
+        ProcessEvent event = mock(ProcessEvent.class);
+        given(event.getEvent()).willReturn(eventType);
+        given(event.getContainer()).willReturn(container);
+        given(event.getCreatedAt()).willReturn(createdAt);
+        return event;
     }
 }
