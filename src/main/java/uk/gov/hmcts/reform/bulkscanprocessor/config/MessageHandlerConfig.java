3ecc3cde9433b75e72d4834a2edb8ad3f1da4d1a
leszekg <gonczar@gmail.com>
2019-02-21 14:30:59 +0000

    Conditionally fail when message handler registration fails

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/MessageHandlerConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/MessageHandlerConfig.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.config;
 
-import com.google.common.util.concurrent.Uninterruptibles;
 import com.microsoft.azure.servicebus.IQueueClient;
 import com.microsoft.azure.servicebus.MessageHandlerOptions;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,11 +14,12 @@ import uk.gov.hmcts.reform.bulkscanprocessor.tasks.ProcessedEnvelopeNotification
 import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 
 @ServiceBusConfiguration
 public class MessageHandlerConfig {
+
+    public static final Logger log = LoggerFactory.getLogger(MessageHandlerConfig.class);
 
     private static final ExecutorService notificationsReadExecutor =
         Executors.newSingleThreadExecutor(r ->
@@ -32,8 +34,8 @@ public class MessageHandlerConfig {
     private static final MessageHandlerOptions messageHandlerOptions =
         new MessageHandlerOptions(1, false, Duration.ofMinutes(5));
 
-    @Value("${WAIT_FOR_QUEUE_CREATION:false}")
-    private boolean waitForQueueCreation;
+    @Value("${FAIL_ON_MESSAGE_HANDLER_REGISTRATION_ERROR:true}")
+    private boolean failOnMessageHandlerRegistrationError;
 
     @Autowired(required = false)
     @Qualifier("read-notifications-client")
@@ -50,18 +52,7 @@ public class MessageHandlerConfig {
     private ProcessedEnvelopeNotificationHandler processedEnvelopeNotificationHandler;
 
     @PostConstruct()
-    public void registerMessageHandlers() {
-        if (waitForQueueCreation) {
-            new Thread(() -> {
-                Uninterruptibles.sleepUninterruptibly(60L, TimeUnit.SECONDS);
-                registerHandlers();
-            }).start();
-        } else {
-            registerHandlers();
-        }
-    }
-
-    private void registerHandlers() {
+    public void registerMessageHandlers() throws InterruptedException, ServiceBusException {
         try {
             if (readNotificationsQueueClient != null) {
                 readNotificationsQueueClient.registerMessageHandler(
@@ -78,16 +69,22 @@ public class MessageHandlerConfig {
             );
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throwRegisterMessageHandlerRegistrationException(e);
+            handleMessageHandlerRegistrationError(e);
         } catch (ServiceBusException e) {
-            throwRegisterMessageHandlerRegistrationException(e);
+            handleMessageHandlerRegistrationError(e);
         }
     }
 
-    private void throwRegisterMessageHandlerRegistrationException(Exception cause) {
-        throw new FailedToRegisterMessageHandlersExcepiton(
-            "An error occurred when trying to register message handlers",
-            cause
-        );
+    private <T extends Exception> void handleMessageHandlerRegistrationError(T cause) throws T {
+        final String errorMessage = "An error occurred when trying to register message handlers";
+
+        if (failOnMessageHandlerRegistrationError) {
+            throw cause;
+        } else {
+            // The application has to keep working on Preview - otherwise the pipeline
+            // wouldn't create queues which it relies on.
+            // The problem will be addresses in BPS-445
+            log.error(errorMessage, cause);
+        }
     }
 }
