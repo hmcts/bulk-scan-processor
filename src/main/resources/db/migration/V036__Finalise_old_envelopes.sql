c6ee46c6a9d7eeaf48496e3b64d0dd88d49f9dfd
leszekg <gonczar@gmail.com>
2019-02-22 14:47:26 +0000

    Prevent duplication and add comment

--- src/main/resources/db/migration/V036__Finalise_old_envelopes.sql
+++ src/main/resources/db/migration/V036__Finalise_old_envelopes.sql
@@ -1,16 +1,27 @@
+-- This script finalises all processed envelopes created before 25th January 2019.
+-- All newer envelopes will be finalised by the service itself.
+-- In production no envelopes were created or processed between 19th and 28th Jan,
+-- so 25th Jan is a safe date. On master, the change was introduced on 24th Jan,
+-- but the script prevents duplicating events.
+
 UPDATE scannable_items
 SET ocrdata = null
 WHERE envelope_id IN
 (
   SELECT id FROM envelopes
-  WHERE status = 'NOTIFICATION_SENT' AND createdat < '2019-01-26'
+  WHERE status = 'NOTIFICATION_SENT' AND createdat < '2019-01-25'
 );
 
 INSERT INTO process_events (container, zipfilename, createdat, event)
-SELECT container, zipFileName, now(), 'COMPLETED'
+SELECT container, zipfilename, now(), 'COMPLETED'
 FROM envelopes
-WHERE status = 'NOTIFICATION_SENT' AND createdat < '2019-01-26';
+WHERE status = 'NOTIFICATION_SENT'
+  AND createdat < '2019-01-25'
+  AND (container, zipfilename) NOT IN (
+    SELECT container, zipfilename FROM process_events
+    WHERE event = 'COMPLETED'
+  );
 
 UPDATE envelopes
 SET status = 'COMPLETED'
-WHERE status = 'NOTIFICATION_SENT' AND createdat < '2019-01-26';
+WHERE status = 'NOTIFICATION_SENT' AND createdat < '2019-01-25';
