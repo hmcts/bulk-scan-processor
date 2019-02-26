209d98c164e3a7ebd431941be36dfcd470624de9
leszekg <gonczar@gmail.com>
2019-02-26 15:56:17 +0000

    Remove information from reason column

--- src/main/resources/db/migration/V037_Ensure_initial_event_exists.sql
+++ src/main/resources/db/migration/V037_Ensure_initial_event_exists.sql
@@ -2,8 +2,8 @@
 -- don't have them, yet, but have other events. This helps create
 -- reports for dates when this event wasn't created by the service.
 
-INSERT INTO process_events (container, zipfilename, createdat, event, reason)
-SELECT container, zipfilename, min(createdat), 'ZIPFILE_PROCESSING_STARTED', 'Created by migration script'
+INSERT INTO process_events (container, zipfilename, createdat, event)
+SELECT container, zipfilename, min(createdat), 'ZIPFILE_PROCESSING_STARTED'
 FROM process_events
 WHERE (container, zipfilename) NOT IN (
   SELECT container, zipfilename FROM process_events
