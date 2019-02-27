3de00e9c1a1cbef793b6ce2772f2f2b758d503dc
Leszek Gonczar <gonczar@gmail.com>
2019-02-27 14:25:40 +0000

    Create 'ZIPFILE_PROCESSING_STARTED' event for old files. (#532)

---
+++ src/main/resources/db/migration/V037__Ensure_initial_event_exists.sql
@@ -0,0 +1,12 @@
+-- This script creates ZIPFILE_PROCESSING_STARTED events for those files that
+-- don't have them, yet, but have other events. This helps create
+-- reports for dates when this event wasn't created by the service.
+
+INSERT INTO process_events (container, zipfilename, createdat, event)
+SELECT container, zipfilename, min(createdat), 'ZIPFILE_PROCESSING_STARTED'
+FROM process_events
+WHERE (container, zipfilename) NOT IN (
+  SELECT container, zipfilename FROM process_events
+  WHERE event = 'ZIPFILE_PROCESSING_STARTED'
+)
+GROUP BY container, zipfilename;
