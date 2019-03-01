8ea7d140ec3038473b9c2246f2806da3d0295f2a
Aliveni Choppa <aliveni.ch@gmail.com>
2019-03-01 14:23:10 +0000

    Drop the index on container and zipfilename columns from process_events table

--- src/main/resources/db/migration/V038__Add_indexes_to_process_events.sql
+++ src/main/resources/db/migration/V038__Add_indexes_to_process_events.sql
@@ -1,2 +1,4 @@
+DROP INDEX process_events_container_zip_idx;
+
 CREATE INDEX process_events_container_zip_event_idx ON process_events (container, zipfilename, event);
 CREATE INDEX process_events_event_createdat_idx ON process_events (event, createdat);
