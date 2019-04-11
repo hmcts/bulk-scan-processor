2584494e5141e8d2e50bcd32886f1cd374b7baf5
donatas M <1420191+doncem@users.noreply.github.com>
2019-04-11 14:57:21 +0100

    Upgrade spring to latest (2.1.4) (#613)
    * Upgrade spring to latest (2.1.4)
    
    * Update plugins
    
    * Enable missed test
    
    * Fix `AuthServiceConfig`
    
    * Move bean override to integration test config

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/AuthServiceConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/AuthServiceConfig.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.bulkscanprocessor.config;
 import org.apache.commons.lang.NotImplementedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -17,16 +16,24 @@ import java.util.List;
 
 @Configuration
 @Lazy
-@EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class AuthServiceConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "idam.s2s-auth.url")
     public AuthTokenGenerator authTokenGenerator(
         @Value("${idam.s2s-auth.secret}") String secret,
         @Value("${idam.s2s-auth.name}") String name,
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, name, serviceAuthorisationApi);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "idam.s2s-auth.url", havingValue = "false")
+    public AuthTokenGenerator authTokenGeneratorStub() {
+        return () -> {
+            throw new NotImplementedException();
+        };
     }
 
     @Bean
