be7507b99ba862347e6867efb2ff391c851c9114
donatas M <1420191+doncem@users.noreply.github.com>
2019-04-30 22:18:02 +0100

    Update shedlock 2.2.1 -> 2.4.0 (#640)
    * Update shedlock 2.2.1 -> 2.4.0
    
    * Introduce zones in cron tasks

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/util/TimeZones.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.util;
+
+public final class TimeZones {
+
+    public static final String EUROPE_LONDON = "Europe/London";
+
+    private TimeZones() {
+        // utility class construct
+    }
+}
