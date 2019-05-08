a13c9decb06e1bea5803fc380980e819bec4a616
Rafal Kondratowicz <rkondr@gmail.com>
2019-05-08 14:07:43 +0200

    Prepare subtype test for upcoming type changes

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/model/mapper/SubtypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/model/mapper/SubtypeTest.java
@@ -10,8 +10,12 @@ import uk.gov.hmcts.reform.bulkscanprocessor.model.common.DocumentSubtype;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.common.DocumentType;
 
 import static java.util.Arrays.asList;
-import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.bulkscanprocessor.helper.EnvelopeCreator.inputScannableItem;
+import static uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputDocumentType.CHERISHED;
+import static uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputDocumentType.COVERSHEET;
+import static uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputDocumentType.OTHER;
+import static uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputDocumentType.SSCS1;
+import static uk.gov.hmcts.reform.bulkscanprocessor.model.blob.InputDocumentType.WILL;
 
 public class SubtypeTest {
 
@@ -21,39 +25,58 @@ public class SubtypeTest {
     @Test
     public void should_map_scannable_item_document_types_correctly() {
         asList(
-            new TestCase(inputScannableItem(InputDocumentType.SSCS1), DocumentType.OTHER, DocumentSubtype.SSCS1),
-            new TestCase(inputScannableItem(InputDocumentType.WILL), DocumentType.OTHER, DocumentSubtype.WILL),
-            new TestCase(inputScannableItem(InputDocumentType.COVERSHEET),
-                DocumentType.OTHER,
-                DocumentSubtype.COVERSHEET
-            ),
-            new TestCase(inputScannableItem(InputDocumentType.CHERISHED),
-                DocumentType.CHERISHED, null),
-            new TestCase(inputScannableItem(InputDocumentType.OTHER), DocumentType.OTHER, null)
-        ).forEach(testCase -> {
+            new TestCase(new Given(SSCS1), new Then(DocumentType.OTHER, DocumentSubtype.SSCS1)),
+            new TestCase(new Given(WILL), new Then(DocumentType.OTHER, DocumentSubtype.WILL)),
+            new TestCase(new Given(COVERSHEET), new Then(DocumentType.OTHER, DocumentSubtype.COVERSHEET)),
+            new TestCase(new Given(CHERISHED), new Then(DocumentType.CHERISHED, null)),
+            new TestCase(new Given(OTHER), new Then(DocumentType.OTHER, null))
+        ).forEach(tc -> {
             // given
-            InputScannableItem item = testCase.input;
+            InputScannableItem item = inputScannableItem(tc.input.documentType);
 
             // when
             ScannableItem result = EnvelopeMapper.toDbScannableItem(item);
 
             // then
-            assertThat(result.getDocumentType()).isEqualTo(testCase.expectedDocType);
-            assertThat(result.getDocumentSubtype()).isEqualTo(testCase.expectedDocSubtype);
+            softly.assertThat(result.getDocumentType())
+                .as("Output document type for type '%s'", tc.input.documentType)
+                .isEqualTo(tc.output.documentType);
+
+            softly.assertThat(result.getDocumentSubtype())
+                .as("Output document subtype for type '%s'", tc.input.documentType)
+                .isEqualTo(tc.output.docSubtype);
         });
     }
 
+    // region helper/dsl classes
     private class TestCase {
-        InputScannableItem input;
-        DocumentType expectedDocType;
-        String expectedDocSubtype;
+        public Given input;
+        public Then output;
 
         // region constructor
-        TestCase(InputScannableItem input, DocumentType expectedDocType, String expectedDocSubtype) {
+        public TestCase(Given input, Then output) {
             this.input = input;
-            this.expectedDocType = expectedDocType;
-            this.expectedDocSubtype = expectedDocSubtype;
+            this.output = output;
         }
         // endregion
     }
+
+    private class Given {
+        public InputDocumentType documentType;
+
+        public Given(InputDocumentType documentType) {
+            this.documentType = documentType;
+        }
+    }
+
+    private class Then {
+        public DocumentType documentType;
+        public String docSubtype;
+
+        public Then(DocumentType documentType, String docSubtype) {
+            this.documentType = documentType;
+            this.docSubtype = docSubtype;
+        }
+    }
+    // endregion
 }
