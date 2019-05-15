851052d7598b1c33b6a16d94c0859da2163b6261
Aliveni Choppa <aliveni.ch@gmail.com>
2019-05-15 16:14:43 +0100

    Removed update script which may cause problems in deployment

--- src/main/resources/db/migration/V040__Add_document_uuid_to_scannable_items.sql
+++ src/main/resources/db/migration/V040__Add_document_uuid_to_scannable_items.sql
@@ -1,9 +1,3 @@
 ALTER TABLE scannable_items
   ADD COLUMN documentUuid VARCHAR(100) NULL;
 
--- Extract the text after the last slash (/) in the documentUrl
-UPDATE scannable_items
-   SET documentUuid = (
-     SELECT documentId[1]
-     FROM
-     REGEXP_MATCHES(documentUrl, '([^/]+)(?=[^/]*/?$)', 'g') AS documentId);
