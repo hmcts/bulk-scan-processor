890c757a33970b3f196840578e8e398f3a2ee8e7
Aliveni Choppa <aliveni.ch@gmail.com>
2019-05-15 15:59:13 +0100

    Updated regex

--- src/main/resources/db/migration/V040__Add_document_uuid_to_scannable_items.sql
+++ src/main/resources/db/migration/V040__Add_document_uuid_to_scannable_items.sql
@@ -6,7 +6,7 @@ UPDATE scannable_items
      SELECT documentId[1] FROM
         REGEXP_MATCHES(
         documenturl,
-        '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}',
+        '([^/]+)(?=[^/]*/?$)',
         'g'
         ) AS documentId
      );
