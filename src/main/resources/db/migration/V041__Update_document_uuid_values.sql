71c9e81cec1c8aab6797e89fe6dcd82d5be6f6d2
Aliveni Choppa <aliveni.ch@gmail.com>
2019-05-16 15:45:03 +0100

    populate documentUuid values only if they are null

--- src/main/resources/db/migration/V041__Update_document_uuid_values.sql
+++ src/main/resources/db/migration/V041__Update_document_uuid_values.sql
@@ -1,3 +1,4 @@
 -- Extracts the text after the last slash (/) in the documentUrl
 UPDATE scannable_items
  SET documentUuid = REVERSE(SPLIT_PART(REVERSE(documentUrl), '/', 1))
+ WHERE documentUuid IS NULL;
