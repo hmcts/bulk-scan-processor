5cee734a9498f3de66364c9132fd91c84b81ea85
Rafal Kondratowicz <rkondr@gmail.com>
2019-07-11 11:02:01 +0100

    Update test

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/ocrvalidation/OcrValidationClientTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/ocrvalidation/OcrValidationClientTest.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.ocrvalidation;
 
+import com.github.tomakehurst.wiremock.core.Options;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.IntegrationContextInitializer;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.IntegrationTest;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.Profiles;
@@ -16,9 +18,11 @@ import uk.gov.hmcts.reform.bulkscanprocessor.ocrvalidation.model.res.ValidationR
 
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.serverError;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 @ActiveProfiles( {
     IntegrationContextInitializer.PROFILE_WIREMOCK,
@@ -31,38 +35,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OcrValidationClientTest {
 
     @Autowired
+    private Options wiremockOptions;
+
+    @Autowired
     private OcrValidationClient client;
 
     @Test
     public void should_map_response_from_service_to_model() {
         // given
-        String responseJson = "{"
-            + "'status' : 'ERRORS',"
-            + "'errors': ['e1', 'e2'],"
-            + "'warnings': '['w1', 'w2']'"
-            + "}";
-
-        stubFor(post("/validate-ocr").willReturn(okJson(responseJson)));
-
-        // when
-        ValidationResponse response = client.validate(
-            "localhost",
-            new FormData(
-                "type",
-                asList(
-                    new OcrDataField("key1", "value1"),
-                    new OcrDataField("key2", "value2")
-                )
-            )
+        stubFor(
+            post("/validate-ocr")
+                .willReturn(okJson(jsonify(
+                    "      {"
+                        + "  'status': 'ERRORS',"
+                        + "  'errors': ['e1', 'e2'],"
+                        + "  'warnings': ['w1', 'w2']"
+                        + "}"
+                )))
         );
 
+        // when
+        ValidationResponse res = client.validate(url(), sampleFormData());
+
         // then
-        assertThat(response.errors).contains("e1", "e2");
-        assertThat(response.warnings).contains("w1", "w2");
-        assertThat(response.status).isEqualTo(Status.ERRORS);
+        assertThat(res.errors).contains("e1", "e2");
+        assertThat(res.warnings).contains("w1", "w2");
+        assertThat(res.status).isEqualTo(Status.ERRORS);
     }
 
     @Test
-    public void should_throw_an_exception() {
+    public void should_throw_an_exception_if_server_responds_with_an_error_code() {
+        // given
+        stubFor(
+            post("/validate-ocr")
+                .willReturn(serverError())
+        );
+
+        // when
+        Throwable err = catchThrowable(() -> client.validate(url(), sampleFormData()));
+
+        // then
+        assertThat(err).isInstanceOf(InternalServerError.class);
+    }
+
+    private FormData sampleFormData() {
+        return new FormData(
+            "type",
+            asList(
+                new OcrDataField("name1", "value1"),
+                new OcrDataField("name2", "value2")
+            )
+        );
+    }
+
+    private String url() {
+        return "http://localhost:" + wiremockOptions.portNumber() + "/validate-ocr";
+    }
+
+    private String jsonify(String s) {
+        return s.replace('\'', '"');
     }
 }
