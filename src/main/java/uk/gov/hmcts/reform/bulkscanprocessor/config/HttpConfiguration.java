c148f3b656611f751023ced12b79e79912530dd1
donatas M <1420191+doncem@users.noreply.github.com>
2019-07-30 15:57:06 +0100

    Update configuration for RestTemplate (#736)
    * Use http client from apache so app insights can track
    
    * Remove overlapping `Bean` - registered in `HttpConfiguration`

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/HttpConfiguration.java
@@ -0,0 +1,44 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.config;
+
+import feign.Client;
+import feign.httpclient.ApacheHttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HttpConfiguration {
+
+    @Bean
+    public Client getFeignHttpClient() {
+        return new ApacheHttpClient(getHttpClient());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory(getHttpClient());
+    }
+
+    private CloseableHttpClient getHttpClient() {
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(30000)
+            .setConnectionRequestTimeout(30000)
+            .setSocketTimeout(60000)
+            .build();
+
+        return HttpClientBuilder
+            .create()
+            .useSystemProperties()
+            .setDefaultRequestConfig(config)
+            .build();
+    }
+}
