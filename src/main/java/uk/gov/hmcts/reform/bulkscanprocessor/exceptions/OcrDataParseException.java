b1047bec5e291bdb83ef3c27f164e17350a59aa3
Leszek Gonczar <gonczar@gmail.com>
2019-08-15 13:47:45 +0100

    Reject envelope when OCR data cannot be parsed (#758)

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrDataParseException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrDataParseException.java
@@ -1,8 +1,19 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
 
-public class OcrDataParseException extends InvalidEnvelopeException {
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
-    public OcrDataParseException(String message, Throwable cause) {
-        super(message, cause);
+/**
+ * Represents a situation where OCR data format is invalid.
+ *
+ * <p>
+ * Needs to extend JsonMappingException, so that we can catch it - otherwise
+ * Jackson would wrap it in its own exception when parsing metadata file.
+ * </p>
+ */
+public class OcrDataParseException extends JsonMappingException {
+
+    public OcrDataParseException(JsonParser jsonParser, String message, Throwable cause) {
+        super(jsonParser, message, cause);
     }
 }
