2175d20205a30761a45d297ed6fd95f8c5048103
leszekg <gonczar@gmail.com>
2019-08-15 13:25:25 +0100

    Add a comment

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrDataParseException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrDataParseException.java
@@ -3,6 +3,14 @@ package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+/**
+ * Represents a situation where OCR data format is invalid.
+ *
+ * <p>
+ * Needs to extend JsonMappingException, so that we can catch it - otherwise
+ * Jackson would wrap it in its own exception when parsing metadata file.
+ * </p>
+ */
 public class OcrDataParseException extends JsonMappingException {
 
     public OcrDataParseException(JsonParser jsonParser, String message, Throwable cause) {
