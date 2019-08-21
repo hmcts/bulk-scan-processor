c634e2dc781a774e71bfd8194b32adb32bf61c80
Rafal Kondratowicz <rkondr@gmail.com>
2019-08-21 15:06:35 +0100

    Checkstyle

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrPresenceException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrPresenceException.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
 
-public class OcrPresenceException extends InvalidEnvelopeException{
+public class OcrPresenceException extends InvalidEnvelopeException {
     public OcrPresenceException(String message) {
         super(message);
     }
