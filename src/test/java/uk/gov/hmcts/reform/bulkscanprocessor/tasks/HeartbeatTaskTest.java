a4411e11f78b56ffdf9032263d36ba9627bcccb0
Rafal Kondratowicz <rkondr@gmail.com>
2019-09-02 12:41:42 +0200

    Add more tests

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/HeartbeatTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/HeartbeatTaskTest.java
@@ -2,20 +2,25 @@ package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.InvalidMessageException;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusHelper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HeartbeatTaskTest {
 
+    @Mock private ServiceBusHelper serviceBusHelper;
+
     @Test
     public void should_send_heartbeat_message_to_the_queue() {
         // given
-        ServiceBusHelper serviceBusHelper = Mockito.mock(ServiceBusHelper.class);
         HeartbeatTask task = new HeartbeatTask(serviceBusHelper);
 
         // when
@@ -23,5 +28,21 @@ public class HeartbeatTaskTest {
 
         // then
         verify(serviceBusHelper).sendMessage(any());
+    }
+
+    @Test
+    public void should_handle_exception_thrown_by_service_bus_client() {
+        // given
+        doThrow(new InvalidMessageException("msg"))
+            .when(serviceBusHelper)
+            .sendMessage(any());
+
+        HeartbeatTask task = new HeartbeatTask(serviceBusHelper);
+
+        // when
+        Throwable exc = catchThrowable(() -> task.run());
+
+        // then
+        assertThat(exc).isNull();
     }
 }
