3d39b5f532f0dea142c296397fb0252bbf0c865a
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-09-02 13:03:48 +0200

    Send heartbeat messages to orchestrator (#784)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/msg/HeartbeatMsg.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg;
+
+import java.util.UUID;
+
+public class HeartbeatMsg implements Msg {
+
+    private final String id;
+
+    public HeartbeatMsg() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    @Override
+    public String getMsgId() {
+        return this.id;
+    }
+
+    @Override
+    public String getLabel() {
+        return MsgLabel.HEARBEAT.toString();
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/HeartbeatTask.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
+
+import net.javacrumbs.shedlock.core.SchedulerLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.HeartbeatMsg;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusHelper;
+
+/**
+ * Sends heartbeat messages to orchestrator.
+ */
+@ConditionalOnProperty(value = "scheduling.task.notifications_to_orchestrator.enabled", matchIfMissing = true)
+@Component
+public class HeartbeatTask {
+
+    private static final Logger log = LoggerFactory.getLogger(HeartbeatTask.class);
+
+    private final ServiceBusHelper serviceBusHelper;
+
+    public HeartbeatTask(@Qualifier("envelopes-helper") ServiceBusHelper serviceBusHelper) {
+        this.serviceBusHelper = serviceBusHelper;
+    }
+
+    @Scheduled(cron = "0 0/10 * * * *")
+    @SchedulerLock(name = "heartbeat")
+    public void run() {
+        try {
+            serviceBusHelper.sendMessage(new HeartbeatMsg());
+            log.info("Heartbeat sent");
+        } catch (Exception exc) {
+            log.error("Error sending heartbeat message", exc);
+        }
+    }
+}
