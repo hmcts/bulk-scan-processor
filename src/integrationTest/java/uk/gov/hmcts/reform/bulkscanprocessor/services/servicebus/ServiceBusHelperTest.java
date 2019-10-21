71eb612c7e086275579a96d5137383b1dd2f99ad
Donatas Martinkus <doncem@users.noreply.github.com>
2019-10-21 10:30:24 +0100

    Fix assertion of iterator

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/services/servicebus/ServiceBusHelperTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/services/servicebus/ServiceBusHelperTest.java
@@ -149,7 +149,9 @@ public class ServiceBusHelperTest {
 
         JsonNode ocrDataValidationWarnings = jsonNode.get("ocr_data_validation_warnings");
         assertThat(ocrDataValidationWarnings.isArray()).isTrue();
-        assertThat(ocrDataValidationWarnings.elements()).asList().containsExactly(new TextNode("warning 1"));
+        assertThat(ocrDataValidationWarnings.elements())
+            .toIterable()
+            .containsExactly(new TextNode("warning 1"));
 
         assertDateField(jsonNode, "delivery_date", message.getDeliveryDate());
         assertDateField(jsonNode, "opening_date", message.getOpeningDate());
