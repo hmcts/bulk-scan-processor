19368c486c1a8dbee10df8212ab878a6a164ae03
Donatas Martinkus <doncem@users.noreply.github.com>
2019-11-11 13:51:46 +0000

    Include `DependsOn` annotation

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ProcessedEnvelopeNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ProcessedEnvelopeNotificationHandler.java
@@ -9,6 +9,7 @@ import com.microsoft.azure.servicebus.IMessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.Profiles;
@@ -31,6 +32,7 @@ import java.util.concurrent.Executors;
  * This involves removing sensitive information, status change and creation of an appropriate event.
  * </p>
  */
+@DependsOn("processed-envelopes-completor")
 @Service
 @Profile(Profiles.NOT_SERVICE_BUS_STUB) // only active when interaction with Service Bus isn't disabled
 public class ProcessedEnvelopeNotificationHandler implements IMessageHandler {
