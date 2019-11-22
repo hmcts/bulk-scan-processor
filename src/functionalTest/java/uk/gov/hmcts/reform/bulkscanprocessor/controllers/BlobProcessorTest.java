17f5b4bce9fa785153b913ded34a739b40bf06f4
Aliveni Choppa <aliveni.ch@gmail.com>
2019-11-22 14:21:19 +0000

    Add functional test for supplementary evidence with ocr classification

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/BlobProcessorTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/BlobProcessorTest.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.reform.bulkscanprocessor.entity.Status;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.out.EnvelopeResponse;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,11 +21,32 @@ public class BlobProcessorTest extends BaseFunctionalTest {
     }
 
     @Test
-    public void should_process_zipfile_after_upload_and_set_status() throws Exception {
+    public void should_process_zipfile_after_upload_and_set_status() {
         List<String> files = Arrays.asList("1111006.pdf", "1111002.pdf");
         String metadataFile = "1111006_2.metadata.json";
         String destZipFilename = testHelper.getRandomFilename("24-06-2018-00-00-00.test.zip");
 
+        // valid zip file
+        EnvelopeResponse envelope = uploadZipFile(files, metadataFile, destZipFilename);
+
+        assertThat(ImmutableList.of(Status.NOTIFICATION_SENT, Status.COMPLETED)).contains(envelope.getStatus());
+        assertThat(envelope.getScannableItems()).hasSize(2);
+        assertThat(envelope.getScannableItems()).noneMatch(item -> Strings.isNullOrEmpty(item.documentUuid));
+    }
+
+    @Test
+    public void should_process_zipfile_with_supplementary_evidence_with_oce_classification() {
+        List<String> files = Collections.singletonList("1111006.pdf");
+        String metadataFile = "1111006_3.metadata.json";
+        String destZipFilename = testHelper.getRandomFilename("24-06-2018-00-00-00.test.zip");
+        EnvelopeResponse envelope = uploadZipFile(files, metadataFile, destZipFilename);
+
+        assertThat(ImmutableList.of(Status.NOTIFICATION_SENT, Status.COMPLETED)).contains(envelope.getStatus());
+        assertThat(envelope.getScannableItems()).hasSize(1);
+        assertThat(envelope.getScannableItems()).noneMatch(item -> Strings.isNullOrEmpty(item.documentUuid));
+    }
+
+    private EnvelopeResponse uploadZipFile(List<String> files, String metadataFile, String destZipFilename) {
         // valid zip file
         testHelper.uploadZipFile(inputContainer, files, metadataFile, destZipFilename, testPrivateKeyDer);
 
@@ -32,10 +54,6 @@ public class BlobProcessorTest extends BaseFunctionalTest {
 
         waitForFileToBeProcessed(destZipFilename, s2sToken);
 
-        EnvelopeResponse envelope = testHelper.getEnvelopeByZipFileName(testUrl, s2sToken, destZipFilename).get();
-
-        assertThat(ImmutableList.of(Status.NOTIFICATION_SENT, Status.COMPLETED)).contains(envelope.getStatus());
-        assertThat(envelope.getScannableItems()).hasSize(2);
-        assertThat(envelope.getScannableItems()).noneMatch(item -> Strings.isNullOrEmpty(item.documentUuid));
+        return testHelper.getEnvelopeByZipFileName(testUrl, s2sToken, destZipFilename).get();
     }
 }
