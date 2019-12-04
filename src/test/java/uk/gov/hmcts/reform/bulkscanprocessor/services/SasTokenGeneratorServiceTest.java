50692ca4a111e380c362a478570c2c024802917b
Donatas Martinkus <doncem@users.noreply.github.com>
2019-12-04 07:52:22 +0000

    Update tests to reflect library upgrade

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/SasTokenGeneratorServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/SasTokenGeneratorServiceTest.java
@@ -52,7 +52,7 @@ public class SasTokenGeneratorServiceTest {
 
         assertThat(queryParams.get("sig")).isNotNull();//this is a generated hash of the resource string
         assertThat(queryParams.get("se")[0]).startsWith(currentDate);//the expiry date/time for the signature
-        assertThat(queryParams.get("sv")).contains("2018-11-09");//azure api version is latest
+        assertThat(queryParams.get("sv")).contains("2019-02-02");//azure api version is latest
         assertThat(queryParams.get("sp")).contains("wl");//access permissions(write-w,list-l)
     }
 
