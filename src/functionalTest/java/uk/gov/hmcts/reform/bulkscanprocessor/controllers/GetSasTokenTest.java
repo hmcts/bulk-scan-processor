537553532b536d0076a9cfeaa91aa0b2bcf64517
Donatas Martinkus <doncem@users.noreply.github.com>
2019-12-04 07:52:22 +0000

    Update tests to reflect library upgrade

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/GetSasTokenTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/GetSasTokenTest.java
@@ -185,7 +185,7 @@ public class GetSasTokenTest {
         Date tokenExpiry = DateUtil.parseDatetime(queryParams.get("se")[0]);
         assertThat(tokenExpiry).isNotNull();
         assertThat(queryParams.get("sig")).isNotNull(); //this is a generated hash of the resource string
-        assertThat(queryParams.get("sv")).contains("2018-11-09"); //azure api version is latest
+        assertThat(queryParams.get("sv")).contains("2019-02-02"); //azure api version is latest
         assertThat(queryParams.get("sp")).contains("wl"); //access permissions(write-w,list-l)
     }
 }
