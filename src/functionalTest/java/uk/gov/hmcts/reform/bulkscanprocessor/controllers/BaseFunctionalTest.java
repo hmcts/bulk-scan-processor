5d7e449c22523c3f4640adf7a49125684cfff787
Nitin Prabhu <prabhunitin61@gmail.com>
2020-01-23 16:28:21 +0000

    Set proxy conditionally

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/BaseFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/BaseFunctionalTest.java
@@ -75,18 +75,6 @@ public abstract class BaseFunctionalTest {
             System.out.format("%s=%s%n", envName, environmentVars.get(envName));
         }
 
-        // This is set temporary and will be removed/modified in subsequent PRs
-        System.setProperty("http.proxyHost", proxyHost);
-        System.setProperty("http.proxyPort", proxyPort);
-
-        System.setProperty("https.proxyHost", proxyHost);
-        System.setProperty("https.proxyPort", proxyPort);
-
-        Map<String, String> environmentVars = System.getenv();
-        for (String envName : environmentVars.keySet()) {
-            System.out.format("%s=%s%n", envName, environmentVars.get(envName));
-        }
-
         CloudBlobClient cloudBlobClient = new CloudStorageAccount(
             storageCredentials,
             new StorageUri(new URI(config.getString("test-storage-account-url")), null),
