f8ef0d4f23b42548280f31fdcdb4a17def602a5d
Rafal Kondratowicz <rkondr@gmail.com>
2020-03-25 10:24:39 +0100

    Fix end hour

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/alerting/NewEnvelopesChecker.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/alerting/NewEnvelopesChecker.java
@@ -20,7 +20,7 @@ public class NewEnvelopesChecker {
 
     public static final Duration TIME_WINDOW = Duration.ofHours(1);
     public static final int START_HOUR = 10;
-    public static final int END_HOUR = 16;
+    public static final int END_HOUR = 17;
 
     private final EnvelopeRepository repo;
     private final Supplier<LocalDateTime> timeSupplier;
