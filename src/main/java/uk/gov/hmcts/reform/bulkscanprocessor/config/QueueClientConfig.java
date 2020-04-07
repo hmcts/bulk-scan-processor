9f12e8ce4aa97649ba7d341a2f3f10250694a858
Donatas Martinkus <doncem@users.noreply.github.com>
2020-04-07 15:14:26 +0100

    Remove reading of error notifications

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/QueueClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/QueueClientConfig.java
@@ -6,7 +6,6 @@ import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -35,15 +34,6 @@ public class QueueClientConfig {
     public IQueueClient notificationsQueueClient(
         @Value("${queues.notifications.connection-string}") String connectionString,
         @Value("${queues.notifications.queue-name}") String queueName
-    ) throws InterruptedException, ServiceBusException {
-        return createQueueClient(connectionString, queueName);
-    }
-
-    @Bean("read-notifications-client")
-    @ConditionalOnProperty(name = "queues.read-notifications.enabled", havingValue = "true")
-    public IQueueClient readNotificationsQueueClient(
-        @Value("${queues.read-notifications.connection-string}") String connectionString,
-        @Value("${queues.read-notifications.queue-name}") String queueName
     ) throws InterruptedException, ServiceBusException {
         return createQueueClient(connectionString, queueName);
     }
--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/ErrorNotificationRepository.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.hmcts.reform.bulkscanprocessor.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ErrorNotificationRepository extends JpaRepository<ErrorNotification, Long> {
-}
