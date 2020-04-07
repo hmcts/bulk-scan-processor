ab038bf44213992b1634b6dc0be496e6ab129022
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2020-04-07 06:30:42 +0100

    Delete job (#1126)
    * DeleteCompleteFilesTask
    
    * Code review
    
    * Handling exception from processing container
    
    * Added using zipDeleted parameter
    
    * Integration test
    
    * Checkstyle
    
    * Fixed PMD

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/DeleteCompleteFilesTask.java
@@ -0,0 +1,134 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
+
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor.BlobManager;
+
+import java.util.List;
+
+import static uk.gov.hmcts.reform.bulkscanprocessor.entity.Status.COMPLETED;
+
+@Service
+public class DeleteCompleteFilesTask {
+    private static final Logger log = LoggerFactory.getLogger(DeleteCompleteFilesTask.class);
+
+    private static final String TASK_NAME = "delete-complete-files";
+
+    private final BlobManager blobManager;
+    private final EnvelopeRepository envelopeRepository;
+
+    public DeleteCompleteFilesTask(
+        BlobManager blobManager,
+        EnvelopeRepository envelopeRepository
+    ) {
+        this.blobManager = blobManager;
+        this.envelopeRepository = envelopeRepository;
+    }
+
+    public void run() {
+        log.info("Started {} task", TASK_NAME);
+
+        for (CloudBlobContainer container : blobManager.listInputContainers()) {
+            try {
+                processCompleteFiles(container);
+            } catch (Exception ex) {
+                log.error(
+                    "Failed to process files from container {}",
+                    container.getName(),
+                    ex
+                );
+            }
+        }
+
+        log.info("Finished {} task", TASK_NAME);
+    }
+
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    private void processCompleteFiles(CloudBlobContainer container) {
+        log.info("Started deleting complete files in container {}", container.getName());
+
+        List<Envelope> envelopes = envelopeRepository.findByContainerAndStatusAndZipDeleted(
+            container.getName(),
+            COMPLETED,
+            false
+        );
+        int successCount = 0;
+        int failureCount = 0;
+        for (Envelope envelope: envelopes) {
+            if (tryProcessCompleteEnvelope(container, envelope)) {
+                successCount++;
+            } else {
+                failureCount++;
+            }
+        }
+
+        log.info(
+            "Finished deleting complete files in container {}, deleted {} files, failed to delete {} files",
+            container.getName(),
+            successCount,
+            failureCount
+        );
+    }
+
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    private boolean tryProcessCompleteEnvelope(CloudBlobContainer container, Envelope envelope) {
+        boolean deleted = false;
+
+        try {
+            CloudBlockBlob cloudBlockBlob = container.getBlockBlobReference(envelope.getZipFileName());
+
+            log.info(
+                "File {} (container {}) is complete - deleting",
+                envelope.getZipFileName(),
+                container.getName()
+            );
+
+            if (cloudBlockBlob.exists()) {
+                deleted = cloudBlockBlob.deleteIfExists();
+                if (deleted) {
+                    log.info(
+                        "Deleted file {} from container {}",
+                        envelope.getZipFileName(),
+                        container.getName()
+                    );
+                } else {
+                    log.info(
+                        "File {} has not been deleted from container {}",
+                        envelope.getZipFileName(),
+                        container.getName()
+                    );
+                }
+            } else {
+                deleted = true;
+                log.info(
+                    "File {} (container {}) has already been deleted.",
+                    envelope.getZipFileName(),
+                    container.getName()
+                );
+            }
+            if (deleted) {
+                envelope.setZipDeleted(true);
+                envelopeRepository.saveAndFlush(envelope);
+                log.info(
+                    "Marked envelope from file {} (container {}) as deleted",
+                    envelope.getZipFileName(),
+                    container.getName()
+                );
+            }
+        } catch (Exception ex) {
+            log.error(
+                "Failed to process file {} from container {}",
+                envelope.getZipFileName(),
+                container.getName(),
+                ex
+            );
+        }
+
+        return deleted;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/DeleteCompleteFilesTaskTest.java
@@ -0,0 +1,268 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
+
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import org.bouncycastle.util.StoreException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.helper.EnvelopeCreator;
+import uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor.BlobManager;
+
+import java.net.URISyntaxException;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static uk.gov.hmcts.reform.bulkscanprocessor.entity.Status.COMPLETED;
+
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("PMD")
+class DeleteCompleteFilesTaskTest {
+
+    @Mock
+    private BlobManager blobManager;
+
+    @Mock
+    private EnvelopeRepository envelopeRepository;
+
+    @Mock
+    private CloudBlobContainer container1;
+
+    private DeleteCompleteFilesTask deleteCompleteFilesTask;
+
+    private static final String CONTAINER_NAME_1 = "container1";
+    private static final String CONTAINER_NAME_2 = "container2";
+
+    @BeforeEach
+    void setUp() {
+        deleteCompleteFilesTask = new DeleteCompleteFilesTask(
+            blobManager,
+            envelopeRepository
+        );
+        given(container1.getName()).willReturn(CONTAINER_NAME_1);
+    }
+
+    @Test
+    void should_delete_single_existing_file() throws Exception {
+        // given
+        final CloudBlockBlob cloudBlockBlob11 = mock(CloudBlockBlob.class);
+
+        final Envelope envelope11 = EnvelopeCreator.envelope("X", COMPLETED, CONTAINER_NAME_1);
+
+        given(blobManager.listInputContainers()).willReturn(singletonList(container1));
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false))
+            .willReturn(singletonList(envelope11));
+        prepareGivensForEnvelope(container1, cloudBlockBlob11, envelope11);
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false);
+        verifyEnvelopesSaving(envelope11);
+        verifyNoMoreInteractions(envelopeRepository);
+
+        verifyCloudBlockBlobInteractions(cloudBlockBlob11);
+    }
+
+    @Test
+    void should_handle_zero_complete_files() {
+        // given
+        given(blobManager.listInputContainers()).willReturn(singletonList(container1));
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false))
+            .willReturn(emptyList());
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verifyZeroInteractions(envelopeRepository);
+    }
+
+    @Test
+    void should_mark_as_deleted_single_non_existing_file() throws Exception {
+        // given
+        final CloudBlockBlob cloudBlockBlob1 = mock(CloudBlockBlob.class);
+
+        final Envelope envelope11 = prepareEnvelope("X", cloudBlockBlob1, container1);
+        given(blobManager.listInputContainers()).willReturn(singletonList(container1));
+        given(cloudBlockBlob1.exists()).willReturn(false);
+        assertThat(envelope11.isZipDeleted()).isFalse();
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false);
+        verifyEnvelopesSaving(envelope11);
+        verifyNoMoreInteractions(envelopeRepository);
+
+        verify(cloudBlockBlob1).exists();
+        verifyNoMoreInteractions(cloudBlockBlob1);
+    }
+
+    @Test
+    void should_handle_not_deleting_existing_file() throws Exception {
+        // given
+        final CloudBlockBlob cloudBlockBlob1 = mock(CloudBlockBlob.class);
+
+        final Envelope envelope11 = prepareEnvelope("X", cloudBlockBlob1, container1);
+        given(blobManager.listInputContainers()).willReturn(singletonList(container1));
+        given(cloudBlockBlob1.exists()).willReturn(true);
+        given(cloudBlockBlob1.deleteIfExists()).willReturn(false);
+        assertThat(envelope11.isZipDeleted()).isFalse();
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verifyZeroInteractions(envelopeRepository);
+
+        verify(cloudBlockBlob1).exists();
+        verify(cloudBlockBlob1).deleteIfExists();
+        verifyNoMoreInteractions(cloudBlockBlob1);
+    }
+
+    @Test
+    void should_not_delete_file_if_exception_thrown() throws Exception {
+        // given
+        final Envelope envelope11 = EnvelopeCreator.envelope("X", COMPLETED, CONTAINER_NAME_1);
+
+        given(blobManager.listInputContainers()).willReturn(singletonList(container1));
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false))
+            .willReturn(singletonList(envelope11));
+        given(container1.getBlockBlobReference(envelope11.getZipFileName()))
+            .willThrow(new StoreException("msg", new RuntimeException()));
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false);
+        verifyNoMoreInteractions(envelopeRepository);
+    }
+
+    @Test
+    void should_process_second_container_if_processing_the_first_container_throws() throws Exception {
+        // given
+        final CloudBlockBlob cloudBlockBlob21 = mock(CloudBlockBlob.class);
+
+        final CloudBlobContainer container2 = mock(CloudBlobContainer.class);
+        given(container2.getName()).willReturn(CONTAINER_NAME_2);
+
+        given(blobManager.listInputContainers()).willReturn(asList(container1, container2));
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false))
+            .willThrow(new RuntimeException("msg"));
+
+        final Envelope envelope21 = prepareEnvelope("Y", cloudBlockBlob21, container2);
+
+        prepareGivensForEnvelope(container2, cloudBlockBlob21, envelope21);
+        assertThat(envelope21.isZipDeleted()).isFalse();
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false);
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_2, COMPLETED, false);
+        verifyEnvelopesSaving(envelope21);
+        verifyNoMoreInteractions(envelopeRepository);
+    }
+
+    @Test
+    void should_delete_multiple_existing_files_in_multiple_containers() throws Exception {
+        // given
+        final CloudBlobContainer container2 = mock(CloudBlobContainer.class);
+        final CloudBlockBlob cloudBlockBlob11 = mock(CloudBlockBlob.class);
+        final CloudBlockBlob cloudBlockBlob12 = mock(CloudBlockBlob.class);
+        final CloudBlockBlob cloudBlockBlob21 = mock(CloudBlockBlob.class);
+        final CloudBlockBlob cloudBlockBlob22 = mock(CloudBlockBlob.class);
+
+        final Envelope envelope11 = EnvelopeCreator.envelope("X", COMPLETED, CONTAINER_NAME_1);
+        final Envelope envelope12 = EnvelopeCreator.envelope("X", COMPLETED, CONTAINER_NAME_1);
+        final Envelope envelope21 = EnvelopeCreator.envelope("Y", COMPLETED, CONTAINER_NAME_2);
+        final Envelope envelope22 = EnvelopeCreator.envelope("Y", COMPLETED, CONTAINER_NAME_2);
+
+        given(blobManager.listInputContainers()).willReturn(asList(container1, container2));
+        given(container2.getName()).willReturn(CONTAINER_NAME_2);
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false))
+            .willReturn(asList(envelope11, envelope12));
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_2, COMPLETED, false))
+            .willReturn(asList(envelope21, envelope22));
+        prepareGivensForEnvelope(container1, cloudBlockBlob11, envelope11);
+        prepareGivensForEnvelope(container1, cloudBlockBlob12, envelope12);
+        prepareGivensForEnvelope(container2, cloudBlockBlob21, envelope21);
+        prepareGivensForEnvelope(container2, cloudBlockBlob22, envelope22);
+
+        // when
+        deleteCompleteFilesTask.run();
+
+        // then
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_1, COMPLETED, false);
+        verify(envelopeRepository).findByContainerAndStatusAndZipDeleted(CONTAINER_NAME_2, COMPLETED, false);
+        verifyEnvelopesSaving(envelope11, envelope12, envelope21, envelope22);
+        verifyNoMoreInteractions(envelopeRepository);
+
+        verifyCloudBlockBlobInteractions(cloudBlockBlob11);
+        verifyCloudBlockBlobInteractions(cloudBlockBlob12);
+        verifyCloudBlockBlobInteractions(cloudBlockBlob21);
+        verifyCloudBlockBlobInteractions(cloudBlockBlob22);
+    }
+
+    private Envelope prepareEnvelope(
+        String jurisdiction,
+        CloudBlockBlob cloudBlockBlob,
+        CloudBlobContainer container
+    ) throws URISyntaxException, StorageException {
+        final Envelope envelope = EnvelopeCreator.envelope(jurisdiction, COMPLETED, container.getName());
+
+        given(envelopeRepository.findByContainerAndStatusAndZipDeleted(container.getName(), COMPLETED, false))
+            .willReturn(singletonList(envelope));
+        given(container.getBlockBlobReference(envelope.getZipFileName())).willReturn(cloudBlockBlob);
+
+        return envelope;
+    }
+
+    private void prepareGivensForEnvelope(
+        CloudBlobContainer container,
+        CloudBlockBlob cloudBlockBlob,
+        Envelope envelope
+    ) throws URISyntaxException, StorageException {
+        given(container.getBlockBlobReference(envelope.getZipFileName())).willReturn(cloudBlockBlob);
+        given(cloudBlockBlob.exists()).willReturn(true);
+        given(cloudBlockBlob.deleteIfExists()).willReturn(true);
+        assertThat(envelope.isZipDeleted()).isFalse();
+    }
+
+    private void verifyCloudBlockBlobInteractions(CloudBlockBlob cloudBlockBlob11) throws StorageException {
+        verify(cloudBlockBlob11).exists();
+        verify(cloudBlockBlob11).deleteIfExists();
+        verifyNoMoreInteractions(cloudBlockBlob11);
+    }
+
+    private void verifyEnvelopesSaving(Envelope... envelopes) {
+        ArgumentCaptor<Envelope> envelopeCaptor = ArgumentCaptor.forClass(Envelope.class);
+        verify(envelopeRepository, times(envelopes.length)).saveAndFlush(envelopeCaptor.capture());
+        for (int i = 0; i < envelopes.length; i++) {
+            assertThat(envelopeCaptor.getAllValues().get(i).getId())
+                .isEqualTo(envelopes[i].getId());
+            assertThat(envelopeCaptor.getAllValues().get(i).getZipFileName())
+                .isEqualTo(envelopes[i].getZipFileName());
+            assertThat(envelopeCaptor.getAllValues().get(i).isZipDeleted()).isTrue();
+        }
+    }
+}
