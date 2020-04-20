8d9e0932fdd2814c7fb2209a6398d27f5aa4b1ae
Rafal Kondratowicz <rkondr@gmail.com>
2020-04-20 19:20:08 +0200

    More cleanup

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/processor/ZipVerifiers.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/processor/ZipVerifiers.java
@@ -1,26 +1,22 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor;
 
 import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.InvalidZipArchiveException;
-import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.InvalidZipFilesException;
 import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.SignatureValidationException;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import static com.google.common.io.ByteStreams.toByteArray;
-import static java.util.Arrays.asList;
 
 public class ZipVerifiers {
 
     public static final String DOCUMENTS_ZIP = "envelope.zip";
     public static final String SIGNATURE_SIG = "signature";
-    public static final String INVALID_SIGNATURE_MESSAGE = "Zip signature failed verification";
 
     private ZipVerifiers() {
     }
@@ -29,7 +25,7 @@ public class ZipVerifiers {
         String signatureAlgorithm
     ) {
         if ("sha256withrsa".equalsIgnoreCase(signatureAlgorithm)) {
-            return ZipVerifiers::verifyAndExtract;
+            return ZipVerifiers::extract;
         } else if ("none".equalsIgnoreCase(signatureAlgorithm)) {
             return ZipVerifiers::noOpVerification;
         }
@@ -41,12 +37,10 @@ public class ZipVerifiers {
     }
 
     /**
-     * Checks whether the wrapping zip has expected entries and returns the internal zip.
+     * Extracts the inner zip.
      */
-    static ZipInputStream verifyAndExtract(ZipStreamWithSignature zipWithSignature) {
+    static ZipInputStream extract(ZipStreamWithSignature zipWithSignature) {
         Map<String, byte[]> zipEntries = extractZipEntries(zipWithSignature.zipInputStream);
-
-        verifyFileNames(zipEntries.keySet());
 
         return new ZipInputStream(new ByteArrayInputStream(zipEntries.get(DOCUMENTS_ZIP)));
     }
@@ -62,14 +56,6 @@ public class ZipVerifiers {
             return zipEntries;
         } catch (IOException ioe) {
             throw new InvalidZipArchiveException("Error extracting zip entries", ioe);
-        }
-    }
-
-    static void verifyFileNames(Set<String> fileNames) {
-        if (!(fileNames.size() == 2 && fileNames.containsAll(asList(DOCUMENTS_ZIP, SIGNATURE_SIG)))) {
-            throw new InvalidZipFilesException(
-                "Zip entries do not match expected file names. Actual names = " + fileNames
-            );
         }
     }
 
--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/processor/ZipVerifiersTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/processor/ZipVerifiersTest.java
@@ -1,53 +1,25 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor;
 
-import com.google.common.collect.ImmutableSet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.InvalidZipFilesException;
 
-import java.util.Set;
+import java.io.ByteArrayInputStream;
+import java.util.zip.ZipInputStream;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.bulkscanprocessor.helper.DirectoryZipper.zipDirAndWrap;
 
 @ExtendWith(MockitoExtension.class)
 public class ZipVerifiersTest {
-
-    private static final String INVALID_ZIP_ENTRIES_MESSAGE = "Zip entries do not match expected file names";
-
     @Test
-    public void should_verify_2_valid_filenames_successfully() throws Exception {
-        Set<String> files = ImmutableSet.of(
-            ZipVerifiers.DOCUMENTS_ZIP,
-            ZipVerifiers.SIGNATURE_SIG
+    public void should_verify_valid_zip_successfully() throws Exception {
+        byte[] zipBytes = zipDirAndWrap("signature/sample_valid_content");
+
+        ZipVerifiers.ZipStreamWithSignature zipStreamWithSig = new ZipVerifiers.ZipStreamWithSignature(
+            new ZipInputStream(new ByteArrayInputStream(zipBytes)), "hello.zip", "some_container"
         );
-
-        assertThatCode(() -> ZipVerifiers.verifyFileNames(files)).doesNotThrowAnyException();
-    }
-
-    @Test
-    public void should_not_verify_more_than_2_files_successfully() throws Exception {
-        Set<String> files = ImmutableSet.of(
-            ZipVerifiers.DOCUMENTS_ZIP,
-            ZipVerifiers.SIGNATURE_SIG,
-            "signature2"
-        );
-
-        assertThatThrownBy(() -> ZipVerifiers.verifyFileNames(files))
-            .isInstanceOf(InvalidZipFilesException.class)
-            .hasMessageContaining(INVALID_ZIP_ENTRIES_MESSAGE);
-    }
-
-    @Test
-    public void should_not_verify_invalid_filenames_successfully() throws Exception {
-        Set<String> files = ImmutableSet.of(
-            ZipVerifiers.DOCUMENTS_ZIP,
-            "not" + ZipVerifiers.SIGNATURE_SIG
-        );
-
-        assertThatThrownBy(() -> ZipVerifiers.verifyFileNames(files))
-            .isInstanceOf(InvalidZipFilesException.class)
-            .hasMessageContaining(INVALID_ZIP_ENTRIES_MESSAGE);
+        ZipInputStream zis = ZipVerifiers.extract(zipStreamWithSig);
+        assertThat(zis).isNotNull();
     }
 }
