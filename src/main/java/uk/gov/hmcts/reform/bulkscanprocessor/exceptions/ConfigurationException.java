a175802b7ad7c477af3c251abe9745acac712c52
Leszek Gonczar <gonczar@gmail.com>
2020-04-27 09:31:31 +0100

    Populate poBox in error notification message (#1185)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/ConfigurationException.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
+
+public class ConfigurationException extends RuntimeException {
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+}
