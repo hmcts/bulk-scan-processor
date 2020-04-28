8d68e080ea8876c86a5a37964a18b7ebb347a315
Rafal Kondratowicz <rkondr@gmail.com>
2020-04-28 12:06:15 +0200

    Remove not used terraform config

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -172,10 +172,6 @@ variable "blob_selected_container" {
   default = "ALL"
 }
 
-variable "blob_signature_verification_key_file" {
-  default = "trusted_public_key.der"
-}
-
 variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
   default     = ""
