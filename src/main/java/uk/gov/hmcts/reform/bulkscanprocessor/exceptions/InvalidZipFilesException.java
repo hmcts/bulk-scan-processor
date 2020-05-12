e71892bd5e6180a8ead42ac7b30944b9e6b2c58c
Leszek Gonczar <gonczar@gmail.com>
2020-05-12 18:48:59 +0100

    Make InvalidZipFilesException extend InvalidEnvelopeException

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/InvalidZipFilesException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/InvalidZipFilesException.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
 
-public class InvalidZipFilesException extends RuntimeException {
+public class InvalidZipFilesException extends InvalidEnvelopeException {
     public InvalidZipFilesException(String message) {
         super(message);
     }
