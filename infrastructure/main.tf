74273a9ef6f803a9d3dad0dc33b485fcef30b111
apereverzin <andrey.pereverzin@hotmail.com>
2020-05-14 21:51:00 +0100

    Added staging db

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,6 +40,21 @@ module "bulk-scan-db" {
   subscription       = "${var.subscription}"
 }
 
+# Staging DB to be used by AAT staging pod for functional tests
+module "bulk-scan-staging-db" {
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product            = "${var.product}-${var.component}-staging"
+  location           = "${var.location_db}"
+  env                = "${var.env}"
+  database_name      = "bulk_scan"
+  postgresql_user    = "bulk_scanner"
+  postgresql_version = "10"
+  sku_name           = "GP_Gen5_2"
+  sku_tier           = "GeneralPurpose"
+  common_tags        = "${var.common_tags}"
+  subscription       = "${var.subscription}"
+}
+
 module "bulk-scan" {
   source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product                         = "${var.product}-${var.component}"
@@ -171,6 +186,38 @@ resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name         = "${var.component}-POSTGRES-DATABASE"
   value        = "${module.bulk-scan-db.postgresql_database}"
 }
+
+# region staging DB secrets
+resource "azurerm_key_vault_secret" "staging_db_user" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "${var.component}-staging-db-user"
+  value        = "${module.bulk-scan-staging-db.user_name}"
+}
+
+resource "azurerm_key_vault_secret" "staging_db_password" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "${var.component}-staging-db-password"
+  value        = "${module.bulk-scan-staging-db.postgresql_password}"
+}
+
+resource "azurerm_key_vault_secret" "staging_db_host" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "${var.component}-staging-db-host"
+  value        = "${module.bulk-scan-staging-db.host_name}"
+}
+
+resource "azurerm_key_vault_secret" "staging_db_port" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "${var.component}-staging-db-port"
+  value        = "${module.bulk-scan-staging-db.postgresql_listen_port}"
+}
+
+resource "azurerm_key_vault_secret" "staging_db_name" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "${var.component}-staging-db-name"
+  value        = "${module.bulk-scan-staging-db.postgresql_database}"
+}
+# endregion
 
 resource "azurerm_key_vault_secret" "notifications_queue_send_conn_string" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
