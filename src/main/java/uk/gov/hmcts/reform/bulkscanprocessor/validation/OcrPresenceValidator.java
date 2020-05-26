1e00e8e19e6f33104a987d97636514c39bbae9af
Rafal Kondratowicz <rkondr@gmail.com>
2020-05-26 11:31:44 +0200

    require docs with OCR to have subtype set

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/validation/OcrPresenceValidator.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/validation/OcrPresenceValidator.java
@@ -18,6 +18,7 @@ public class OcrPresenceValidator {
     public static final String MULTIPLE_OCR_MSG = "Multiple docs with OCR";
     public static final String MISSING_OCR_MSG = "Empty OCR on 'form' document";
     public static final String MISPLACED_OCR_MSG = "OCR on document of invalid type";
+    public static final String MISSING_DOC_SUBTYPE_MSG = "Missing subtype on document with OCR";
 
     // The only document types that can (and must) have OCR data.
     // Note: remove 'SSCS1' once sscs migrates to the new format.
@@ -38,6 +39,9 @@ public class OcrPresenceValidator {
         }
         if (docs.stream().anyMatch(doc -> OCR_DOC_TYPES.contains(doc.documentType) && doc.ocrData == null)) {
             throw new OcrPresenceException(MISSING_OCR_MSG);
+        }
+        if (docs.stream().filter(doc -> doc.ocrData != null).anyMatch(doc -> doc.documentSubtype == null)) {
+            throw new OcrPresenceException(MISSING_DOC_SUBTYPE_MSG);
         }
 
         return docs
