1bf54d602b8e211744d493bea6400e9593e79f22
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-05-27 18:15:40 +0200

    Remove reupload job (#1298)

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/UploadEnvelopeDocumentsTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/UploadEnvelopeDocumentsTaskTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
@@ -10,11 +12,12 @@ import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
 import uk.gov.hmcts.reform.bulkscanprocessor.model.common.Classification;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.UploadEnvelopeDocumentsService;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static java.time.Instant.now;
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -23,6 +26,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static uk.gov.hmcts.reform.bulkscanprocessor.entity.Status.CREATED;
+import static uk.gov.hmcts.reform.bulkscanprocessor.entity.Status.UPLOAD_FAILURE;
 
 @ExtendWith(MockitoExtension.class)
 class UploadEnvelopeDocumentsTaskTest {
@@ -31,20 +35,24 @@ class UploadEnvelopeDocumentsTaskTest {
     private static final String CONTAINER_2 = "container-2";
     private static final String ZIP_FILE_NAME = "zip-file-name";
 
+    @Captor ArgumentCaptor<List<Envelope>> envelopesCaptor;
+
     @Mock private EnvelopeRepository envelopeRepository;
     @Mock private UploadEnvelopeDocumentsService uploadService;
+    private final int maxRetries = 5;
 
     private UploadEnvelopeDocumentsTask task;
 
     @BeforeEach
     void setUp() {
-        task = new UploadEnvelopeDocumentsTask(envelopeRepository, uploadService);
+        task = new UploadEnvelopeDocumentsTask(envelopeRepository, uploadService, maxRetries);
     }
 
     @Test
     void should_do_nothing_when_no_envelopes_to_process_are_found() {
         // given
-        given(envelopeRepository.findByStatus(CREATED)).willReturn(emptyList());
+        given(envelopeRepository.findByStatusIn(asList(CREATED, UPLOAD_FAILURE)))
+            .willReturn(emptyList());
 
         // when
         task.run();
@@ -59,11 +67,12 @@ class UploadEnvelopeDocumentsTaskTest {
     @Test
     void should_call_service_twice_when_2_different_containers_are_present_in_the_list() {
         // given
-        List<Envelope> envelopes = Arrays.asList(
-            getEnvelope(CONTAINER_1),
-            getEnvelope(CONTAINER_2)
+        List<Envelope> envelopes = asList(
+            getEnvelope(CONTAINER_1, 0),
+            getEnvelope(CONTAINER_2, 1)
         );
-        given(envelopeRepository.findByStatus(CREATED)).willReturn(envelopes);
+        given(envelopeRepository.findByStatusIn(asList(CREATED, UPLOAD_FAILURE)))
+            .willReturn(envelopes);
 
         // when
         task.run();
@@ -75,8 +84,30 @@ class UploadEnvelopeDocumentsTaskTest {
         verify(uploadService, times(2)).processByContainer(anyString(), anyList());
     }
 
-    private Envelope getEnvelope(String containerName) {
-        return new Envelope(
+    @Test
+    void should_not_send_envelopes_that_failed_too_many_times() {
+        // given
+        List<Envelope> envelopes = asList(
+            getEnvelope(CONTAINER_1, maxRetries - 1),
+            getEnvelope(CONTAINER_1, maxRetries),
+            getEnvelope(CONTAINER_1, maxRetries + 1)
+        );
+        given(envelopeRepository.findByStatusIn(asList(CREATED, UPLOAD_FAILURE)))
+            .willReturn(envelopes);
+
+        // when
+        task.run();
+
+        // then
+        verify(uploadService, times(1)).processByContainer(anyString(), envelopesCaptor.capture());
+        List<Envelope> processedEnvelopes = envelopesCaptor.getValue();
+        // only the first envelopes should be processed
+        assertThat(processedEnvelopes).hasSize(1);
+        assertThat(processedEnvelopes.get(0).getUploadFailureCount()).isEqualTo(maxRetries - 1);
+    }
+
+    private Envelope getEnvelope(String containerName, int uploadFailures) {
+        Envelope envelope = new Envelope(
             "po-box",
             "jurisdiction",
             now(), // delivery date
@@ -91,5 +122,8 @@ class UploadEnvelopeDocumentsTaskTest {
             emptyList(),
             containerName
         );
+        envelope.setUploadFailureCount(uploadFailures);
+
+        return envelope;
     }
 }
