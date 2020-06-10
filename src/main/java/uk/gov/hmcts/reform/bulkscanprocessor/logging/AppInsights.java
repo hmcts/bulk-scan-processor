a7e7ec0120899b9263f3fb4814f5d6b5f9500f88
mdayican <mustafa.dayican@hmcts.net>
2020-06-10 18:23:00 +0100

    remove log import

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/logging/AppInsights.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/logging/AppInsights.java
@@ -7,8 +7,6 @@ import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import static java.lang.System.currentTimeMillis;
