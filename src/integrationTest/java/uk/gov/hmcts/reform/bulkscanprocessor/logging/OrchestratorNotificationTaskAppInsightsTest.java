5d20204e2c468fee510f7b8eb1b4970d2f48a99f
mdayican <mustafa.dayican@hmcts.net>
2020-06-10 23:48:02 +0100

    remove shedlock from notification and upload tasks

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/logging/OrchestratorNotificationTaskAppInsightsTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/logging/OrchestratorNotificationTaskAppInsightsTest.java
@@ -7,34 +7,36 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEventRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.logging.OrchestratorNotificationTaskAppInsightsTest.MockConfig;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusHelper;
 import uk.gov.hmcts.reform.bulkscanprocessor.tasks.OrchestratorNotificationTask;
 
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.bulkscanprocessor.entity.Status.UPLOADED;
 
 @TestPropertySource(
     properties = {
-        "scheduling.task.notifications_to_orchestrator.delay=99999",
         "scheduling.task.notifications_to_orchestrator.enabled=true"
     }
 )
-@SpringBootTest
+@ContextConfiguration(classes = {OrchestratorNotificationTask.class, AppInsights.class, MockConfig.class})
 @RunWith(SpringRunner.class)
 public class OrchestratorNotificationTaskAppInsightsTest {
 
@@ -54,10 +56,10 @@ public class OrchestratorNotificationTaskAppInsightsTest {
     public void should_trace_when_success() throws InterruptedException {
 
         given(envelopeRepository.findByStatus(UPLOADED)).willReturn(Arrays.asList());
+        doNothing().when(telemetry).trackRequest(any());
 
         orchestratorNotificationTask.run();
 
-        TimeUnit.SECONDS.sleep(1);
         verify(telemetry, atLeastOnce()).trackRequest(telemetryRequestCaptor.capture());
 
         RequestTelemetry requestTelemetry = telemetryRequestCaptor.getValue();
@@ -70,13 +72,14 @@ public class OrchestratorNotificationTaskAppInsightsTest {
     @Test
     public void should_trace_when_failed() throws InterruptedException {
         given(envelopeRepository.findByStatus(UPLOADED)).willThrow(new RuntimeException("failed"));
+        doNothing().when(telemetry).trackRequest(any());
 
         try {
             orchestratorNotificationTask.run();
         } catch (Exception ex) {
             //ignore
         }
-        TimeUnit.SECONDS.sleep(1);
+
         verify(telemetry, atLeastOnce()).trackRequest(telemetryRequestCaptor.capture());
 
         RequestTelemetry requestTelemetry = telemetryRequestCaptor.getValue();
@@ -86,12 +89,19 @@ public class OrchestratorNotificationTaskAppInsightsTest {
         assertThat(requestTelemetry.getId()).isNotBlank();
     }
 
-    @TestConfiguration
+    @Configuration
     public static class MockConfig {
-
         @Bean(name = "envelopes-helper")
         public ServiceBusHelper envelopesQueueHelper() {
             return mock(ServiceBusHelper.class);
         }
+        @Bean
+        public EnvelopeRepository envelopeRepo() {
+            return mock(EnvelopeRepository.class);
+        }
+        @Bean
+        public ProcessEventRepository processEventRepo() { return mock(ProcessEventRepository.class); }
+        @Bean
+        public TelemetryClient telemetryClient() { return mock(TelemetryClient.class); }
     }
 }
--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/logging/UploadEnvelopeDocumentsTaskAppInsightsTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/logging/UploadEnvelopeDocumentsTaskAppInsightsTest.java
@@ -7,30 +7,36 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.logging.UploadEnvelopeDocumentsTaskAppInsightsTest.MockConfig;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.UploadEnvelopeDocumentsService;
 import uk.gov.hmcts.reform.bulkscanprocessor.tasks.UploadEnvelopeDocumentsTask;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @TestPropertySource(
     properties = {
-        "scheduling.task.upload-documents.delay=99999",
         "scheduling.task.upload-documents.enabled=true",
         "scheduling.task.upload-documents.max_tries=3"
     }
 )
-@SpringBootTest
+@ContextConfiguration(classes = {UploadEnvelopeDocumentsTask.class, AppInsights.class, MockConfig.class})
 @RunWith(SpringRunner.class)
 public class UploadEnvelopeDocumentsTaskAppInsightsTest {
 
@@ -48,11 +54,10 @@ public class UploadEnvelopeDocumentsTaskAppInsightsTest {
 
     @Test
     public void should_trace_when_success() throws InterruptedException {
-
         given(envelopeRepository.findEnvelopesToUpload(3)).willReturn(Arrays.asList());
+        doNothing().when(telemetry).trackRequest(any());
 
         uploadEnvelopeDocumentsTask.run();
-        TimeUnit.SECONDS.sleep(1);
 
         verify(telemetry, atLeastOnce()).trackRequest(telemetryRequestCaptor.capture());
 
@@ -66,13 +71,13 @@ public class UploadEnvelopeDocumentsTaskAppInsightsTest {
     @Test
     public void should_trace_when_failed() throws InterruptedException {
         given(envelopeRepository.findEnvelopesToUpload(3)).willThrow(new RuntimeException("failed"));
+        doNothing().when(telemetry).trackRequest(any());
 
         try {
             uploadEnvelopeDocumentsTask.run();
         } catch (Exception ex) {
             //ignore
         }
-        TimeUnit.SECONDS.sleep(1);
 
         verify(telemetry, atLeastOnce()).trackRequest(telemetryRequestCaptor.capture());
 
@@ -81,5 +86,17 @@ public class UploadEnvelopeDocumentsTaskAppInsightsTest {
         assertThat(requestTelemetry.getName()).isEqualTo("Schedule /UploadEnvelopeDocumentsTask");
         assertThat(requestTelemetry.isSuccess()).isFalse();
         assertThat(requestTelemetry.getId()).isNotBlank();
+    }
+
+    @Configuration
+    public static class MockConfig {
+        @Bean
+        public EnvelopeRepository envelopeRepo() {
+            return mock(EnvelopeRepository.class);
+        }
+        @Bean
+        public UploadEnvelopeDocumentsService uploadService() { return mock(UploadEnvelopeDocumentsService.class); }
+        @Bean
+        public TelemetryClient telemetryClient() { return mock(TelemetryClient.class); }
     }
 }
