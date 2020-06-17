9962c474ac382f02e559d902a4fb356a3feceec9
mdayican <mustafa.dayican@hmcts.net>
2020-06-17 12:40:20 +0100

    send ocr errors in notification messages (#1349)

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrValidationException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/OcrValidationException.java
@@ -1,7 +1,21 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
 
 public class OcrValidationException extends InvalidMetafileException {
+
+    // might contain sensitive data
+    private final String detailMessage;
+
     public OcrValidationException(String message) {
         super(message);
+        detailMessage = message;
+    }
+
+    public OcrValidationException(String message, String detailMessage) {
+        super(message);
+        this.detailMessage = detailMessage;
+    }
+
+    public String getDetailMessage() {
+        return detailMessage;
     }
 }
