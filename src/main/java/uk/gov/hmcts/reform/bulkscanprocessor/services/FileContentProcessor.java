5265c66e6f3adf751a8e4099fad52812cff346de
apereverzin <andrey.pereverzin@hotmail.com>
2020-06-23 16:56:50 +0100

    FileContentProcessor

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/FileContentProcessor.java
@@ -0,0 +1,4 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services;
+
+public class FileContentProcessor {
+}
---
+++ src/test/uk/gov/hmcts/reform/bulkscanprocessor/services/FileContentProcessorTest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileContentProcessorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void processZipFileContent() {
+    }
+}
