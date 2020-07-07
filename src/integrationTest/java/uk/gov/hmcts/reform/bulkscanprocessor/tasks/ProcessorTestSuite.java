f4802d77c04c733923b9c3c6f1e35214bed426c7
apereverzin <andrey.pereverzin@hotmail.com>
2020-07-07 16:28:27 +0100

    FileNamesExtractor

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ProcessorTestSuite.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/ProcessorTestSuite.java
@@ -25,6 +25,7 @@ import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.ErrorMsg;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.EnvelopeHandler;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.ErrorNotificationSender;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.FileContentProcessor;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.FileNamesExtractor;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.FileRejector;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.document.DocumentManagementService;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus.ServiceBusHelper;
@@ -115,6 +116,7 @@ public abstract class ProcessorTestSuite {
     protected CloudBlobContainer rejectedContainer;
 
     private static DockerComposeContainer dockerComposeContainer;
+    private FileNamesExtractor fileNamesExtractor;
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -123,6 +125,8 @@ public abstract class ProcessorTestSuite {
         CloudBlobClient cloudBlobClient = account.createCloudBlobClient();
 
         blobManager = new BlobManager(cloudBlobClient, blobManagementProperties);
+
+        fileNamesExtractor = new FileNamesExtractor();
 
         documentProcessor = new DocumentProcessor(
             documentManagementService,
@@ -170,6 +174,7 @@ public abstract class ProcessorTestSuite {
 
         processor = new BlobProcessorTask(
             blobManager,
+            fileNamesExtractor,
             envelopeProcessor,
             fileContentProcessor
         );
