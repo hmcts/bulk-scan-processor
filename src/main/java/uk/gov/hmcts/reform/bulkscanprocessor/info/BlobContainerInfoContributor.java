175ac64d6853acb46087180d058704acc86ad2d6
mdayican <mustafa.dayican@hmcts.net>
2020-08-05 16:27:05 +0100

    add test for contributor - for /info endpoint - blob lib upgrade part 2.1 (#1444)

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/info/BlobContainerInfoContributor.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/info/BlobContainerInfoContributor.java
@@ -4,14 +4,11 @@ import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.models.BlobContainerItem;
 import org.springframework.boot.actuate.info.Info.Builder;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.bulkscanprocessor.config.Profiles;
 
 import static java.util.stream.Collectors.toList;
 
 @Component
-@Profile(Profiles.NOT_STORAGE_STUB)
 public class BlobContainerInfoContributor implements InfoContributor {
 
     private final BlobServiceClient client;
