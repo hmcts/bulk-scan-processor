1fbeb954ddd8870bb16f843981c4b1fcf2cda1be
mdayican <mustafa.dayican@hmcts.net>
2020-08-11 15:15:12 +0100

    introduce new classes to lease and check-mark metadata (#1451)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/storage/LeaseAcquirer.java
@@ -0,0 +1,99 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services.storage;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.models.BlobErrorCode;
+import com.azure.storage.blob.models.BlobStorageException;
+import com.azure.storage.blob.specialized.BlobLeaseClient;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+import static com.azure.storage.blob.models.BlobErrorCode.BLOB_NOT_FOUND;
+import static com.azure.storage.blob.models.BlobErrorCode.LEASE_ALREADY_PRESENT;
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Component
+public class LeaseAcquirer {
+
+    public static final int LEASE_DURATION_IN_SECONDS = 60;
+    private static final Logger logger = getLogger(LeaseAcquirer.class);
+
+    private final LeaseClientProvider leaseClientProvider;
+    private final LeaseMetaDataChecker leaseMetaDataChecker;
+
+    public LeaseAcquirer(
+        LeaseClientProvider leaseClientProvider,
+        LeaseMetaDataChecker leaseMetaDataChecker
+    ) {
+        this.leaseClientProvider = leaseClientProvider;
+        this.leaseMetaDataChecker = leaseMetaDataChecker;
+    }
+
+    /**
+     * Main wrapper for blobs to be leased by {@link BlobLeaseClient}.
+     * @param blobClient Represents blob
+     * @param onLeaseSuccess Consumer which takes in {@code leaseId} acquired with {@link BlobLeaseClient}
+     * @param onFailure Extra step to execute in case an error occurred
+     * @param releaseLease Flag weather to release the lease or not
+     */
+    public void ifAcquiredOrElse(
+        BlobClient blobClient,
+        Consumer<String> onLeaseSuccess,
+        Consumer<BlobErrorCode> onFailure,
+        boolean releaseLease
+    ) {
+        try {
+            var leaseClient = leaseClientProvider.get(blobClient);
+            var leaseId = leaseClient.acquireLease(LEASE_DURATION_IN_SECONDS);
+            boolean isReady = false;
+
+            try {
+                isReady = leaseMetaDataChecker.isReadyToUse(blobClient, leaseId);
+            } catch (Exception ex) {
+                logger.warn(
+                    "Could not check meta data for lease expiration on file {} in container {}",
+                    blobClient.getBlobName(),
+                    blobClient.getContainerName()
+                );
+            } finally {
+                if (!isReady) {
+                    release(leaseClient, blobClient);
+                }
+            }
+
+            if (isReady) {
+                onLeaseSuccess.accept(leaseId);
+                if (releaseLease) {
+                    release(leaseClient, blobClient);
+                }
+            }
+        } catch (BlobStorageException exc) {
+            if (exc.getErrorCode() != LEASE_ALREADY_PRESENT && exc.getErrorCode() != BLOB_NOT_FOUND) {
+                logger.error(
+                    "Error acquiring lease for blob. File name: {}, Container: {}",
+                    blobClient.getBlobName(),
+                    blobClient.getContainerName(),
+                    exc
+                );
+            }
+
+            onFailure.accept(exc.getErrorCode());
+        }
+    }
+
+    private void release(BlobLeaseClient leaseClient, BlobClient blobClient) {
+        try {
+            leaseClient.releaseLease();
+        } catch (BlobStorageException exc) {
+            logger.warn(
+                "Could not release the lease with ID {}. Blob: {}, container: {}",
+                leaseClient.getLeaseId(),
+                blobClient.getBlobName(),
+                blobClient.getContainerName(),
+                exc
+            );
+        }
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/storage/LeaseAcquirerTest.java
@@ -0,0 +1,135 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services.storage;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.models.BlobErrorCode;
+import com.azure.storage.blob.models.BlobStorageException;
+import com.azure.storage.blob.specialized.BlobLeaseClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.function.Consumer;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("unchecked")
+class LeaseAcquirerTest {
+
+    @Mock private BlobClient blobClient;
+    @Mock private BlobLeaseClient leaseClient;
+    @Mock private BlobStorageException blobStorageException;
+
+    @Mock private LeaseMetaDataChecker leaseMetaDataChecker;
+
+    private LeaseAcquirer leaseAcquirer;
+
+    @BeforeEach
+    void setUp() {
+        leaseAcquirer = new LeaseAcquirer(blobClient -> leaseClient, leaseMetaDataChecker);
+    }
+
+    @Test
+    void should_run_provided_action_when_lease_was_acquired() {
+        // given
+        var onSuccess = mock(Consumer.class);
+        var onFailure = mock(Consumer.class);
+
+        given(leaseMetaDataChecker.isReadyToUse(any(),any())).willReturn(true);
+
+        // when
+        leaseAcquirer.ifAcquiredOrElse(blobClient, onSuccess, onFailure, false);
+
+        // then
+        verify(onSuccess).accept(null);
+        verify(onFailure, never()).accept(any(BlobErrorCode.class));
+    }
+
+    @Test
+    void should_run_onFailure_action_when_there_is_error() {
+        // given
+        var onSuccess = mock(Consumer.class);
+        var onFailure = mock(Consumer.class);
+
+        doThrow(blobStorageException).when(leaseClient).acquireLease(anyInt());
+
+        // when
+        leaseAcquirer.ifAcquiredOrElse(blobClient, onSuccess, onFailure, false);
+
+        // then
+        verify(onSuccess, never()).accept(anyString());
+        verify(onFailure).accept(null);
+    }
+
+    @Test
+    void should_not_call_release_when_failed_to_process_blob() {
+        // given
+        doThrow(blobStorageException).when(leaseClient).acquireLease(anyInt());
+
+        // when
+        leaseAcquirer.ifAcquiredOrElse(blobClient, mock(Consumer.class), mock(Consumer.class), true);
+
+        // then
+        verify(leaseClient, never()).releaseLease();
+    }
+
+    @Test
+    void should_call_release_when_successfully_processed_blob() {
+        //given
+        given(leaseMetaDataChecker.isReadyToUse(any(),any())).willReturn(true);
+
+        // when
+        leaseAcquirer.ifAcquiredOrElse(blobClient, mock(Consumer.class), mock(Consumer.class), true);
+
+        // then
+        verify(leaseClient).releaseLease();
+    }
+
+
+    @Test
+    void should_not_run_provided_action_when_metadata_lease_was_not_ready() {
+        // given
+        var onSuccess = mock(Consumer.class);
+        var onFailure = mock(Consumer.class);
+
+        given(leaseMetaDataChecker.isReadyToUse(any(),any())).willReturn(false);
+
+        // when
+        leaseAcquirer.ifAcquiredOrElse(blobClient, onSuccess, onFailure, false);
+
+        // then
+        verify(leaseClient).releaseLease();
+        verify(onSuccess, never()).accept(anyString());
+        verify(onFailure, never()).accept(any());
+    }
+
+
+    @Test
+    void should_release_lease_when_metadata_lease_check_was_throw_exception() {
+        // given
+        var onSuccess = mock(Consumer.class);
+        var onFailure = mock(Consumer.class);
+
+        given(leaseMetaDataChecker.isReadyToUse(any(),any()))
+            .willThrow(new RuntimeException("Can not write to Metadata"));
+
+        // when
+        leaseAcquirer.ifAcquiredOrElse(blobClient, onSuccess, onFailure, true);
+
+        // then
+        verify(leaseClient).releaseLease();
+        verify(onSuccess, never()).accept(anyString());
+        verify(onFailure, never()).accept(any());
+
+    }
+
+}
