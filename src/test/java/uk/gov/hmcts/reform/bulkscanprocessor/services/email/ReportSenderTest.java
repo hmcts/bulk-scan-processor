55f0a2643f67cd99a442d3a5399fc7576a70ce70
apereverzin <andrey.pereverzin@hotmail.com>
2020-08-12 15:15:39 +0100

    Inlined method

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/email/ReportSenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/email/ReportSenderTest.java
@@ -66,7 +66,7 @@ public class ReportSenderTest {
         assertThat(msg.getSubject()).isEqualTo(ReportSender.EMAIL_SUBJECT);
         assertThat(msg.getPlainContent()).isEqualTo(ReportSender.EMAIL_BODY);
         assertThat(msg.getAttachmentList()).hasSize(1);
-        LocalDate yesterday = getYesterday();
+        LocalDate yesterday = LocalDate.now().minusDays(1);
         assertThat(msg.getAttachmentList().get(0).getName())
             .isEqualTo(ReportSender.ATTACHMENT_PREFIX + yesterday + ".csv");
 
@@ -107,9 +107,5 @@ public class ReportSenderTest {
         props.put("mail.transport.protocol", ServerSetupTest.SMTP.getProtocol());
 
         return mailSender;
-    }
-
-    private LocalDate getYesterday() {
-        return LocalDate.now().minusDays(1);
     }
 }
