883d18a8b2f5c9c2dc60b821a03a579550b14d1c
mdayican <mustafa.dayican@hmcts.net>
2020-08-12 14:55:22 +0100

    use new lib in rejected file cleaning part-4 (#1448)

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/CleanUpRejectedFilesTaskTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/tasks/CleanUpRejectedFilesTaskTest.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.tasks;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.models.BlobItem;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.testcontainers.containers.DockerComposeContainer;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.BlobManagementProperties;
 import uk.gov.hmcts.reform.bulkscanprocessor.config.IntegrationTest;
+import uk.gov.hmcts.reform.bulkscanprocessor.services.storage.LeaseAcquirer;
 import uk.gov.hmcts.reform.bulkscanprocessor.tasks.processor.BlobManager;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.bulkscanprocessor.services.storage.LeaseMetaDataChecker.LEASE_EXPIRATION_TIME;
+import static uk.gov.hmcts.reform.bulkscanprocessor.util.TimeZones.EUROPE_LONDON_ZONE_ID;
 
 @IntegrationTest
 public class CleanUpRejectedFilesTaskTest {
@@ -23,7 +33,14 @@ public class CleanUpRejectedFilesTaskTest {
     @Autowired
     private BlobManagementProperties blobManagementProperties;
 
-    private CloudBlobContainer rejectedContainer;
+    @Autowired
+    private LeaseAcquirer leaseAcquirer;
+
+    @Autowired
+    private BlobServiceClient blobServiceClient;
+
+    private BlobContainerClient rejectedContainer;
+
     private BlobManager blobManager;
 
     private static DockerComposeContainer dockerComposeContainer;
@@ -44,33 +61,78 @@ public class CleanUpRejectedFilesTaskTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        CloudStorageAccount account = CloudStorageAccount.parse("UseDevelopmentStorage=true");
-        CloudBlobClient cloudBlobClient = account.createCloudBlobClient();
 
-        this.blobManager = new BlobManager(null, cloudBlobClient, blobManagementProperties);
+        this.blobManager = new BlobManager(blobServiceClient, null, blobManagementProperties);
 
-        this.rejectedContainer = cloudBlobClient.getContainerReference("test-rejected");
-        this.rejectedContainer.createIfNotExists();
+        this.rejectedContainer = blobServiceClient.getBlobContainerClient(("test-rejected"));
+        if (!this.rejectedContainer.exists()) {
+            this.rejectedContainer.create();
+        }
+    }
+
+    @AfterEach
+    protected void deleteAllContainers() {
+        this.rejectedContainer.delete();
     }
 
     @Test
-    public void should_delete_old_files() throws Exception {
+    public void should_delete_old_files_with_snapshots() throws Exception {
         // given
         // there are two files in rejected container
-        rejectedContainer.getBlockBlobReference("foo.zip").uploadText("some content");
-        rejectedContainer.getBlockBlobReference("bar.zip").uploadText("some content");
+        uploadFile(rejectedContainer, "foo.zip", "content_foo_1");
+        uploadFile(rejectedContainer, "bar.zip", "content_bar_2");
 
         assertThat(rejectedContainer.listBlobs()).hasSize(2); // sanity check
 
-        // one of them has a snapshot
+        Map<String, String> blobMetaData = new HashMap<>();
+        // add expired leaseExpirationTime to metadata
+        blobMetaData.put(LEASE_EXPIRATION_TIME, LocalDateTime.now(EUROPE_LONDON_ZONE_ID).minusSeconds(10).toString());
+        // one of them has a snapshot and expired lease metadata
         rejectedContainer
-            .getBlockBlobReference("bar.zip")
-            .createSnapshot();
+            .getBlobClient("bar.zip")
+            .createSnapshot()
+            .setMetadata(blobMetaData);
+
 
         // when
-        new CleanUpRejectedFilesTask(blobManager, "PT0H").run();
+        new CleanUpRejectedFilesTask(blobManager, leaseAcquirer, "PT0H").run();
 
         // then
         assertThat(rejectedContainer.listBlobs()).isEmpty();
+    }
+
+    @Test
+    public void should_not_delete_old_file_if_metadata_lease_not_expired() throws Exception {
+        // given
+        // there are two files in rejected container
+        uploadFile(rejectedContainer, "foo.zip", "content_foo");
+        uploadFile(rejectedContainer, "bar.zip", "content_bar");
+
+        assertThat(rejectedContainer.listBlobs()).hasSize(2); // sanity check
+
+        Map<String, String> blobMetaData = new HashMap<>();
+        // add non-expired leaseExpirationTime to metadata
+        blobMetaData.put(LEASE_EXPIRATION_TIME, LocalDateTime.now(EUROPE_LONDON_ZONE_ID).plusSeconds(90).toString());
+
+        // one of them has vslid lease metadata
+        rejectedContainer
+            .getBlobClient("bar.zip")
+            .setMetadata(blobMetaData);
+
+        // when
+        new CleanUpRejectedFilesTask(blobManager, leaseAcquirer, "PT0H").run();
+
+        // then
+        assertThat(rejectedContainer.listBlobs()).isNotEmpty();
+        List<BlobItem> listBlobItem = rejectedContainer.listBlobs().stream().collect(Collectors.toList());
+        assertThat(listBlobItem.size()).isEqualTo(1);
+        assertThat(listBlobItem.get(0).getName()).isEqualTo("bar.zip");
+
+    }
+
+    private void uploadFile(BlobContainerClient containerClient, String fileName, String content) {
+        byte[] byteContent = content.getBytes();
+        containerClient.getBlobClient(fileName)
+            .upload(new ByteArrayInputStream(byteContent), byteContent.length);
     }
 }
