b47b90197d6917e6b4c28dfc8475334d6ece6c7e
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-08-19 12:23:12 +0100

    Revert "Try NOT to use local docker compose."

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/EnvelopeControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/EnvelopeControllerTest.java
@@ -105,7 +105,8 @@ public class EnvelopeControllerTest {
         File dockerComposeFile = new File("src/integrationTest/resources/docker-compose.yml");
 
         dockerComposeContainer = new DockerComposeContainer(dockerComposeFile)
-            .withExposedService("azure-storage", 10000);
+            .withExposedService("azure-storage", 10000)
+            .withLocalCompose(true);
 
         dockerComposeContainer.start();
         dockerHost = dockerComposeContainer.getServiceHost("azure-storage", 10000);
