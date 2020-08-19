16838f72f7d1085d8b8b680f197159088e7e1d02
donatas M <1420191+doncem@users.noreply.github.com>
2020-08-19 12:54:22 +0100

    Remove no longer used `EnvelopeStatusChangeValidator` (#1481)

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/InvalidStatusChangeException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
-
-public class InvalidStatusChangeException extends RuntimeException {
-
-    public InvalidStatusChangeException(String message) {
-        super(message);
-    }
-}
