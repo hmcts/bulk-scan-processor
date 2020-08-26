a8fbbcce36af49402b114a1762e2fa7320a77e7e
Rafal Kondratowicz <rkondr@gmail.com>
2020-08-26 13:43:32 +0200

    Use enum in json response

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/reports/models/DiscrepancyType.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/reports/models/DiscrepancyType.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.services.reports.models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum DiscrepancyType {
+
     REPORTED_BUT_NOT_RECEIVED("reported but not received"),
     RECEIVED_BUT_NOT_REPORTED("received but not reported"),
     PAYMENT_DCNS_MISMATCH("payment dcns mismatch"),
     SCANNABLE_DOCUMENT_DCNS_MISMATCH("scannable document dcns mismatch");
 
+    @JsonValue
     public final String text;
 
     DiscrepancyType(String text) {
