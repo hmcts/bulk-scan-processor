3b4caa38748fea8ae09cf8e35c27431d85482dde
apereverzin <andrey.pereverzin@hotmail.com>
2020-09-24 13:50:55 +0100

    Readiness config

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/BlobDeleteException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/exceptions/BlobDeleteException.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.bulkscanprocessor.exceptions;
 public class BlobDeleteException extends RuntimeException {
 
     public BlobDeleteException(String message) {
+        //
         super(message);
     }
 }
