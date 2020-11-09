ba76ae72acefe520ed3b9ccc06da43fdd08d8ed0
MUSTAFA <mustafa.dayican@hmcts.net>
2020-11-09 14:41:29 +0000

    fix style

--- src/smokeTest/java/uk/gov/hmcts/reform/bulkscanprocessor/ApplicationHealthTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscanprocessor/ApplicationHealthTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.bulkscanprocessor;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
