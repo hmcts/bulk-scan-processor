06074de43fb280d041b070f5ef7e5ba6be8a4b97
Andrey Pereverzin <andrey.pereverzin@hotmail.com>
2021-01-14 09:13:51 +0000

    Initial implementation of EnvelopeReprocessService (#1668)
    * Initial implementation of EnvelopeReprocessService
    
    * Added test

---
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/EnvelopeReprocessServiceTest.java
@@ -0,0 +1,69 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.entity.ProcessEventRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.EnvelopeNotFoundException;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class EnvelopeReprocessServiceTest {
+    private EnvelopeReprocessService envelopeReprocessService;
+
+    @Mock
+    private EnvelopeRepository envelopeRepository;
+
+    @Mock
+    private ProcessEventRepository processEventRepository;
+
+    @BeforeEach
+    void setUp() {
+        envelopeReprocessService = new EnvelopeReprocessService(
+            envelopeRepository,
+            processEventRepository
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_envelope_does_not_exist() {
+        // given
+        UUID uuid = UUID.randomUUID();
+
+        given(envelopeRepository.findById(uuid)).willReturn(Optional.empty());
+
+        // when
+        // then
+        assertThatThrownBy(() ->
+            envelopeReprocessService.reprocessEnvelope(uuid.toString())
+        )
+            .isInstanceOf(EnvelopeNotFoundException.class)
+            .hasMessageContaining("Envelope with id " + uuid.toString() + " not found");
+    }
+
+    @Test
+    void should_not_throw_exception_if_envelope_exists() {
+        // given
+        UUID uuid = UUID.randomUUID();
+        Envelope envelope = mock(Envelope.class);
+        given(envelopeRepository.findById(uuid)).willReturn(Optional.of(envelope));
+
+        // when
+        // then
+        assertThatCode(() ->
+            envelopeReprocessService.reprocessEnvelope(uuid.toString())
+        )
+            .doesNotThrowAnyException();
+    }
+}
