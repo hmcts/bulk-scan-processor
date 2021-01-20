590761c21836f24c505ecc8c8b2ddf70400e09f8
mdayican <mustafa.dayican@hmcts.net>
2021-01-20 15:15:02 +0000

    fix test

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/SasTokenControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/SasTokenControllerTest.java
@@ -67,7 +67,7 @@ public class SasTokenControllerTest {
 
         assertThat(queryParams.get("sig")).isNotNull();//this is a generated hash of the resource string
         assertThat(queryParams.get("se")).startsWith(currentDate);//the expiry date/time for the signature
-        assertThat(queryParams.get("sv")).contains("2020-02-10");//azure api version is latest
+        assertThat(queryParams.get("sv")).contains("2020-04-08");//azure api version is latest
         assertThat(queryParams.get("sp")).contains("wl");//access permissions(write-w,list-l)
     }
 }
