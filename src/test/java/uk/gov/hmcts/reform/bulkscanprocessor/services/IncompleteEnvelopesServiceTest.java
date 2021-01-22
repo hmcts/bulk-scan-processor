d627735fa26b79bd9bcf35d6eaf44476b73fce1b
apereverzin <andrey.pereverzin@hotmail.com>
2021-01-22 13:20:18 +0000

    Changed return type

--- src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/IncompleteEnvelopesServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanprocessor/services/IncompleteEnvelopesServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.Envelope;
 import uk.gov.hmcts.reform.bulkscanprocessor.entity.EnvelopeRepository;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.BlobInfo;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,8 +15,10 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static uk.gov.hmcts.reform.bulkscanprocessor.entity.Status.COMPLETED;
 import static uk.gov.hmcts.reform.bulkscanprocessor.helper.EnvelopeCreator.envelope;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,26 +36,34 @@ class IncompleteEnvelopesServiceTest {
     @Test
     void should_propagate_result_from_repository() {
         // given
-        List<Envelope> envelopes = asList(envelope(), envelope());
+        List<Envelope> envelopes = asList(
+            envelope("file1.zip", "CMC", COMPLETED, emptyList(), "cmc"),
+            envelope("file2.zip", "SSCS", COMPLETED, emptyList(), "sscs")
+        );
         given(envelopeRepository.getIncompleteEnvelopesBefore(any(LocalDateTime.class)))
             .willReturn(envelopes);
 
         // when
-        List<Envelope> result = incompleteEnvelopesService.getIncompleteEnvelopes(2);
+        List<BlobInfo> result = incompleteEnvelopesService.getIncompleteEnvelopes(2);
 
         // then
-        assertThat(result).isEqualTo(envelopes);
+        assertThat(result)
+            .extracting(e -> tuple(e.fileName, e.container))
+            .containsExactlyInAnyOrder(
+                tuple("file1.zip", "cmc"),
+                tuple("file2.zip", "sscs")
+            )
+        ;
     }
 
     @Test
     void should_propagate_empty_result() {
         // given
-        List<Envelope> envelopes = asList(envelope(), envelope());
         given(envelopeRepository.getIncompleteEnvelopesBefore(any(LocalDateTime.class)))
             .willReturn(emptyList());
 
         // when
-        List<Envelope> result = incompleteEnvelopesService.getIncompleteEnvelopes(2);
+        List<BlobInfo> result = incompleteEnvelopesService.getIncompleteEnvelopes(2);
 
         // then
         assertThat(result).isEmpty();
