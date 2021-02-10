e68eb45c1eaa43ad6885243683804e0c82b306ab
apereverzin <andrey.pereverzin@hotmail.com>
2021-02-10 15:36:04 +0000

    Added lastmodified column

--- src/main/resources/db/migration/V059__Add_lastmodified_column_to_envelopes.sql
+++ src/main/resources/db/migration/V059__Add_lastmodified_column_to_envelopes.sql
@@ -1,13 +1,13 @@
+ALTER TABLE envelopes
+  ADD COLUMN lastmodified TIMESTAMPTZ NULL;
+
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-  NEW.updated_at = NOW();
+  NEW.lastmodified = NOW();
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
-ALTER TABLE envelopes
-  ADD COLUMN lastmodified TIMESTAMPTZ NULL;
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON envelopes
