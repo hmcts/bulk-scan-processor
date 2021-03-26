153d06dd3fc100f780ae17599fd58083d2ea1075
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2021-03-26 09:18:04 +0000

    DTSBPS-444: Payment repository changes (#1803)
    * DTSBPS-444: Payment repository changes
    
    * DTSBPS-444: Payment repository changes

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/PaymentRepository.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/PaymentRepository.java
@@ -1,9 +1,20 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Payment p SET p.status = 'SUBMITTED'"
+        + "WHERE p.documentControlNumber in :dcn")
+    int updateStatus(@Param("dcn") List<String> dcns);
+
+    Optional<List<Payment>> findByDocumentControlNumberIn(List<String> paymentDcns);
 }
