fbae90edf8cbac8e65286a77b76611bbaebec313
mdayican <mustafa.dayican@hmcts.net>
2021-04-08 15:36:48 +0100

    clean up tests

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/PaymentRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/PaymentRepositoryTest.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.entity;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,11 +30,11 @@ class PaymentRepositoryTest {
     @Autowired
     private EnvelopeRepository envelopeRepository;
 
-    @BeforeEach
+    @AfterEach
     public void cleanUp() {
         envelopeRepository.deleteAll();
     }
-
+    
     @Test
     void should_update_payment_status() {
 
