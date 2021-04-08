d1bc710b01171d4125626b0e01767ea5831d9b0b
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2021-04-08 15:17:37 +0100

    DTSBPS-479: Fixing payment controller response (#1822)
    * DTSBPS-479: Fixing payment controller response
    
    * DTSBPS-479: Fixing payment controller response
    
    * DTSBPS-479: Fixing payment controller response
    
    * DTSBPS-479: Fixing payment controller response
    
    * DTSBPS-479: Fixing failing test

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/PaymentControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/PaymentControllerTest.java
@@ -23,7 +23,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -67,7 +66,7 @@ public class PaymentControllerTest {
             .content(request))
             .andDo(print())
             .andExpect(status().isOk())
-            .andExpect(content().string(PaymentController.SUCCESSFUL_UPATE));
+            .andExpect(jsonPath("$.status").value("Payment status successfully updated"));
 
         //Then
         verify(authService, times(1)).authenticate("testServiceAuthHeader");
