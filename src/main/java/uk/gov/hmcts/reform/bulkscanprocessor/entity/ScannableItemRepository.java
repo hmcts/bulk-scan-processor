e6012530b7aa13067b8c0d5bb0d70c8c108b148f
gauravchawla <gaurav-chawla@hotmail.com>
2021-04-13 20:09:00 +0100

    changes made for getting envelopeids from db

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/ScannableItemRepository.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/ScannableItemRepository.java
@@ -1,9 +1,28 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ScannableItemRepository extends JpaRepository<ScannableItem, UUID> {
+
+    /**
+     * Finds envelope with a blob not deleted for a given container and zip file name.
+     *
+     * @param documentControllNumber of scannable item.
+     * @return A list of envelopes.
+     */
+    @Query("select e.zipFileName from scannable_items si"
+        + " JOIN envelopes e on e.id = si.envelope_id"
+        + " where si.documentControlNumber like :documentControllNumber"
+        + " order by si.scanningDate desc"
+    )
+
+    List<String> findByDcn(
+        @Param("documentControllNumber") String documentControllNumber
+    );
 
 }
