0853f964cf2ea951b646a0166cadf745df50dda4
gauravchawla <gaurav-chawla@hotmail.com>
2021-04-19 12:24:52 +0100

    modifications

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/ZipStatusController.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/ZipStatusController.java
@@ -1,13 +1,11 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.controllers;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.bulkscanprocessor.model.out.zipfilestatus.ZipFileStatus;
 import uk.gov.hmcts.reform.bulkscanprocessor.services.zipfilestatus.ZipFileStatusService;
 
 import java.util.Optional;
@@ -28,12 +26,13 @@ public class ZipStatusController {
     // endregion
 
     @GetMapping
-    public ResponseEntity<?> getStatus(@RequestParam(value = "name", required = false) Optional<String> fileName, @RequestParam(value = "ccd_id", required = false) Optional<String> ccdId) {
+    public ResponseEntity<?> getStatus(
+        @RequestParam(value = "name", required = false) Optional<String> fileName,
+        @RequestParam(value = "ccd_id", required = false) Optional<String> ccdId) {
 
-        if (fileName.isPresent() && !ccdId.isPresent()){
-           return ResponseEntity.ok(service.getStatusFor(fileName.get()));
-        }
-        else if (!fileName.isPresent() && ccdId.isPresent()){
+        if (fileName.isPresent() && !ccdId.isPresent()) {
+            return ResponseEntity.ok(service.getStatusFor(fileName.get()));
+        } else if (!fileName.isPresent() && ccdId.isPresent()) {
             return ResponseEntity.ok(service.getStatusByCcdId(ccdId.get()));
         }
         return ResponseEntity.badRequest().body(null);
