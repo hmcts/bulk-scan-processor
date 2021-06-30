4afe57d2add76eba089f67fcc8a88f12efaf399e
MUSTAFA <mustafa.dayican@hmcts.net>
2021-06-30 10:41:06 +0100

    DTSBPS-649 Change notification queue client (#1983)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/servicebus/ServiceBusSendHelper.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.services.servicebus;
+
+import com.azure.core.util.BinaryData;
+import com.azure.messaging.servicebus.ServiceBusException;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import uk.gov.hmcts.reform.bulkscanprocessor.exceptions.InvalidMessageException;
+import uk.gov.hmcts.reform.bulkscanprocessor.model.out.msg.Msg;
+
+public class ServiceBusSendHelper {
+
+    private final ServiceBusSenderClient sendClient;
+
+    private final ObjectMapper objectMapper;
+
+    public ServiceBusSendHelper(ServiceBusSenderClient sendClient, ObjectMapper objectMapper) {
+        this.sendClient = sendClient;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendMessage(Msg msg) {
+        ServiceBusMessage busMessage = mapToBusMessage(msg);
+        try {
+            sendClient.sendMessage(busMessage);
+        } catch (ServiceBusException e) {
+            throw new InvalidMessageException("Unable to send message", e);
+        }
+    }
+
+    ServiceBusMessage mapToBusMessage(Msg msg) {
+        if (msg == null) {
+            throw new InvalidMessageException("Msg == null");
+        }
+        if (Strings.isNullOrEmpty(msg.getMsgId())) {
+            throw new InvalidMessageException("Msg Id == null");
+        }
+        var serviceBusMessage = new ServiceBusMessage(BinaryData.fromString(getMsgBodyInString(msg)));
+        serviceBusMessage.setContentType("application/json");
+        serviceBusMessage.setMessageId(msg.getMsgId());
+        serviceBusMessage.setSubject(msg.getLabel());
+
+        return serviceBusMessage;
+    }
+
+    private String getMsgBodyInString(Msg message) {
+        try {
+            return objectMapper.writeValueAsString(message); //default encoding is UTF-8
+        } catch (JsonProcessingException e) {
+            throw new InvalidMessageException("Unable to create message body in json format", e);
+        }
+    }
+}
+
