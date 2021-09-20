4cffff9a7a9e43cdbb82ce766952d27c915434a2
mdayican <mustafa.dayican@hmcts.net>
2021-09-20 21:12:35 +0100

    DTSBPS-739 fix integrations

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/AuthConfig.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.bulkscanprocessor.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
+
+@Configuration
+@EnableFeignClients(clients = IdamApi.class)
+public class AuthConfig {
+
+    @Bean
+    public AuthTokenGenerator authTokenGenerator(
+        @Value("${idam.s2s-auth.secret}") String secret,
+        @Value("${idam.s2s-auth.name}") String name,
+        ServiceAuthorisationApi serviceAuthorisationApi
+    ) {
+        return AuthTokenGeneratorFactory.createDefaultGenerator(secret, name, serviceAuthorisationApi);
+    }
+}
