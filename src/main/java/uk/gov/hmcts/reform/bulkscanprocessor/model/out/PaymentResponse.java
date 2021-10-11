adcda9ab91f05845fe56ce97279118690a253dd9
MUSTAFA <mustafa.dayican@hmcts.net>
2021-10-11 12:21:18 +0100

    DTSBPS-763 add payment details (#2113)

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/PaymentResponse.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/out/PaymentResponse.java
@@ -1,15 +1,34 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.model.out;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import uk.gov.hmcts.reform.bulkscanprocessor.util.InstantSerializer;
+
+import java.time.Instant;
+import java.util.UUID;
 
 public class PaymentResponse {
+
+    public final UUID id;
+
+    public final String status;
+
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonProperty("last_modified")
+    public Instant lastmodified;
 
     @JsonProperty("document_control_number")
     public final String documentControlNumber;
 
     public PaymentResponse(
-        @JsonProperty("document_control_number") String documentControlNumber
-    ) {
+        @JsonProperty("id") UUID id,
+        @JsonProperty("document_control_number") String documentControlNumber,
+        @JsonProperty("status") String status,
+        @JsonProperty("last_modified") Instant lastmodified) {
+        this.id = id;
         this.documentControlNumber = documentControlNumber;
+        this.status = status;
+        this.lastmodified = lastmodified;
     }
+
 }
