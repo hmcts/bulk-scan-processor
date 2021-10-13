691947c7fc92d1eea9fd6012e9c6250ba83847c8
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-10-13 13:25:35 +0100

    Search by dcn

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/ProcessEventRepository.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/entity/ProcessEventRepository.java
@@ -1,9 +1,27 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProcessEventRepository extends JpaRepository<ProcessEvent, Long> {
     List<ProcessEvent> findByZipFileName(String zipFileName);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * "
+                    + "FROM process_events "
+                    + "WHERE zipFileName LIKE :dcnPrefix"
+                    + "% "
+                    + "AND DATE(createdAt) BETWEEN :fromDate AND :toDate "
+                    + "ORDER BY zipFileName, createdAt DESC"
+    )
+    List<ProcessEvent> findEventsByDcnPrefix(
+            @Param("dcnPrefix") String dcnPrefix,
+            @Param("fromDate") LocalDate fromDate,
+            @Param("toDate") LocalDate toDate
+    );
 }
