be7428c4ea706231c7b7ae3db40ceac4c214b444
alivenichoppa <97441560+alivenichoppa@users.noreply.github.com>
2022-03-31 17:02:57 +0100

    Cleanup main.tf file (#2312)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -81,84 +81,7 @@ module "bulk-scan-staging-db" {
   subscription       = var.subscription
 }
 
-module "bulk-scan" {
-  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product                         = "${var.product}-${var.component}"
-  location                        = "${var.location}"
-  env                             = "${var.env}"
-  ilbIp                           = "${var.ilbIp}"
-  subscription                    = "${var.subscription}"
-  is_frontend                     = "false"
-  capacity                        = "${var.capacity}"
-  common_tags                     = "${var.common_tags}"
-  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
-  instance_size                   = "${local.sku_size}"
-  asp_name                        = "${var.product}-${var.env}"
-  asp_rg                          = "${var.product}-${var.env}"
-  java_container_version          = "9.0"
-  enable_ase                      = "${var.enable_ase}"
-
-  app_settings = {
-    // db
-    BULK_SCANNING_DB_HOST         = "${module.bulk-scan-db.host_name}"
-    BULK_SCANNING_DB_PORT         = "${module.bulk-scan-db.postgresql_listen_port}"
-    BULK_SCANNING_DB_USER_NAME    = "${module.bulk-scan-db.user_name}"
-    BULK_SCANNING_DB_PASSWORD     = "${module.bulk-scan-db.postgresql_password}"
-    BULK_SCANNING_DB_NAME         = "${module.bulk-scan-db.postgresql_database}"
-    BULK_SCANNING_DB_CONN_OPTIONS = "${local.db_connection_options}"
-    FLYWAY_URL                    = "jdbc:postgresql://${module.bulk-scan-db.host_name}:${module.bulk-scan-db.postgresql_listen_port}/${module.bulk-scan-db.postgresql_database}${local.db_connection_options}"
-    FLYWAY_USER                   = "${module.bulk-scan-db.user_name}"
-    FLYWAY_PASSWORD               = "${module.bulk-scan-db.postgresql_password}"
-    FLYWAY_NOOP_STRATEGY          = "true"
-
-    STORAGE_ACCOUNT_NAME  = "${local.storage_account_name}"
-    STORAGE_KEY           = "${local.storage_account_primary_key}"
-    STORAGE_URL           = "${local.storage_account_url}"
-    STORAGE_PROXY_ENABLED = "${var.storage_proxy_enabled}"
-    SAS_TOKEN_VALIDITY    = "${var.token_validity}"
-
-    DOCUMENT_MANAGEMENT_URL = "${local.dm_store_url}"
-
-    S2S_URL    = "${local.s2s_url}"
-    S2S_NAME   = "${var.s2s_name}"
-    S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-
-    SCAN_DELAY         = "${var.scan_delay}"
-    SCAN_ENABLED       = "${var.scan_enabled}"
-
-    NOTIFICATIONS_TO_ORCHESTRATOR_TASK_ENABLED = "${var.orchestrator_notifications_task_enabled}"
-    NOTIFICATIONS_TO_ORCHESTRATOR_TASK_DELAY   = "${var.orchestrator_notifications_task_delay}"
-
-    DELETE_REJECTED_FILES_ENABLED = "${var.delete_rejected_files_enabled}"
-    DELETE_REJECTED_FILES_CRON    = "${var.delete_rejected_files_cron}"
-    DELETE_REJECTED_FILES_TTL     = "${var.delete_rejected_files_ttl}"
-
-    STORAGE_BLOB_LEASE_TIMEOUT               = "${var.blob_lease_timeout}"               // In seconds
-
-    STORAGE_BLOB_SELECTED_CONTAINER = "${var.blob_selected_container}"
-
-    SMTP_HOST          = "${var.smtp_host}"
-    SMTP_USERNAME      = "${data.azurerm_key_vault_secret.smtp_username.value}"
-    SMTP_PASSWORD      = "${data.azurerm_key_vault_secret.smtp_password.value}"
-    REPORTS_CRON       = "${var.reports_cron}"
-    REPORTS_RECIPIENTS = "${data.azurerm_key_vault_secret.reports_recipients.value}"
-
-    INCOMPLETE_ENVELOPES_TASK_CRON    = "${var.incomplete_envelopes_cron}"
-    INCOMPLETE_ENVELOPES_TASK_ENABLED = "${var.incomplete_envelopes_enabled}"
-
-    PROCESS_PAYMENTS_ENABLED          = "${var.process_payments_enabled}"
-
-    OCR_VALIDATION_URL_BULKSCAN_SAMPLE_APP = "${var.ocr_validation_url_bulkscan_sample_app}"
-    OCR_VALIDATION_URL_PROBATE             = "${var.ocr_validation_url_probate}"
-
-    NO_NEW_ENVELOPES_TASK_ENABLED     = "false"
-
-    // silence the "bad implementation" logs
-    LOGBACK_REQUIRE_ALERT_LEVEL = "false"
-    LOGBACK_REQUIRE_ERROR_CODE  = "false"
-  }
-}
-
+# region: key vault definitions
 data "azurerm_key_vault" "key_vault" {
   name                = "${local.vaultName}"
   resource_group_name = "${local.vaultName}"
@@ -173,7 +96,9 @@ data "azurerm_key_vault" "reform_scan_key_vault" {
   name                = "reform-scan-${local.local_env}"
   resource_group_name = "reform-scan-${local.local_env}"
 }
+# endregion
 
+# region DB secrets
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "${var.component}-POSTGRES-USER"
@@ -233,6 +158,15 @@ resource "azurerm_key_vault_secret" "POSTGRES_DATABASE-V11" {
   name         = "${var.component}-POSTGRES-DATABASE-V11"
   value        = "${module.bulk-scan-db-v11.postgresql_database}"
 }
+# endregion
+
+# Copy postgres password for flyway migration
+resource "azurerm_key_vault_secret" "flyway_password" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "flyway-password"
+  value        = "${module.bulk-scan-db.postgresql_password}"
+}
+# endregion
 
 # region staging DB secrets
 resource "azurerm_key_vault_secret" "staging_db_user" {
@@ -266,6 +200,7 @@ resource "azurerm_key_vault_secret" "staging_db_name" {
 }
 # endregion
 
+# region: notification queue access key
 resource "azurerm_key_vault_secret" "notifications_queue_send_access_key" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "notifications-queue-send-shared-access-key"
@@ -277,12 +212,16 @@ resource "azurerm_key_vault_secret" "notifications_queue_send_access_key_premium
   name         = "notifications-queue-send-shared-access-key-premium"
   value        = "${data.azurerm_key_vault_secret.notifications_queue_send_access_key_premium.value}"
 }
+# endregion
 
+# region: copy s2s secret to bulk-scan key vault
 data "azurerm_key_vault_secret" "s2s_secret" {
   key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
   name         = "microservicekey-bulk-scan-processor"
 }
+# endregion
 
+# region: storage secrets
 data "azurerm_key_vault_secret" "storage_account_name" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "storage-account-name"
@@ -292,7 +231,9 @@ data "azurerm_key_vault_secret" "storage_account_primary_key" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "storage-account-primary-key"
 }
+# endregion
 
+# region: copy notification queue secrets to bulk-scan key vault
 data "azurerm_key_vault_secret" "notifications_queue_send_access_key" {
   key_vault_id = "${data.azurerm_key_vault.reform_scan_key_vault.id}"
   name         = "notification-queue-send-shared-access-key"
@@ -302,8 +243,9 @@ data "azurerm_key_vault_secret" "notifications_queue_send_access_key_premium" {
   key_vault_id = "${data.azurerm_key_vault.reform_scan_key_vault.id}"
   name         = "notification-queue-send-shared-access-key-premium"
 }
+# endregion
 
-
+# region: reports secrets
 data "azurerm_key_vault_secret" "reports_recipients" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "reports-recipients"
@@ -318,15 +260,10 @@ data "azurerm_key_vault_secret" "smtp_password" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "reports-email-password"
 }
-
-# Copy postgres password for flyway migration
-resource "azurerm_key_vault_secret" "flyway_password" {
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "flyway-password"
-  value        = "${module.bulk-scan-db.postgresql_password}"
-}
+# endregion
 
 # TODO: remove V10-BCKP secrets after moving to V11 database
+# region: DB backup secrets
 resource "azurerm_key_vault_secret" "POSTGRES-USER-V10-BCKP" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
   name         = "${var.component}-POSTGRES-USER-V10-BCKP"
@@ -362,3 +299,4 @@ resource "azurerm_key_vault_secret" "flyway_password_v10_bckp" {
   name         = "flyway-password-v10-bckp"
   value        = "${module.bulk-scan-db.postgresql_password}"
 }
+# endregion
