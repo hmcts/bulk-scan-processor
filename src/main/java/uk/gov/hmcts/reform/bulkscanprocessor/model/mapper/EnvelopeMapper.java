5d34e8ee2eb2b7649b15d41d99500d490ec5f592
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2022-10-12 15:11:48 +0100

    update subtype mapping

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/mapper/EnvelopeMapper.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/model/mapper/EnvelopeMapper.java
@@ -30,7 +30,6 @@ public class EnvelopeMapper {
     private static final Map<InputDocumentType, String> subtypeMapping =
         Map.of(
             InputDocumentType.SSCS1, DocumentSubtype.SSCS1,
-            InputDocumentType.WILL, DocumentSubtype.WILL,
             InputDocumentType.COVERSHEET, DocumentSubtype.COVERSHEET
         );
 
