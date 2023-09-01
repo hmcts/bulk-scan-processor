152c4d5b48554870f709fcf86d9f219e19ca1370
Imran Ali <Imran.Ali@justice.gov.uk>
2023-09-01 12:13:27 +0100

    pmdMain fix

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/launchdarkly/Flags.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/launchdarkly/Flags.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.launchdarkly;
 
 public final class Flags {
+
+    public static final String BULK_SCAN_PROCESSOR_TEST = "bulk-scan-processor-test";
+
     private Flags() {
 
     }
-
-    public static final String BULK_SCAN_PROCESSOR_TEST = "bulk-scan-processor-test";
 }
