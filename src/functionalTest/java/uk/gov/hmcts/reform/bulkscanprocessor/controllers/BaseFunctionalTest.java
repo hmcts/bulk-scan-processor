6b5f32a59a7449866d7bfee314a2c5b56b541f68
Imran Ali <Imran.Ali@justice.gov.uk>
2023-09-12 16:40:15 +0100

    reverted timer change

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/BaseFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/BaseFunctionalTest.java
@@ -66,7 +66,7 @@ public abstract class BaseFunctionalTest {
         String s2sToken = testHelper.s2sSignIn(S2S_NAME, S2S_SECRET, S2S_URL);
 
         await("File " + fileName + " should be processed")
-            .atMost(SCAN_DELAY + 600_000, TimeUnit.MILLISECONDS)
+            .atMost(SCAN_DELAY + 60_000, TimeUnit.MILLISECONDS)
             .pollInterval(500, TimeUnit.MILLISECONDS)
             .until(
                 () -> {
