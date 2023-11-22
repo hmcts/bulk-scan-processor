c2045e2ddde3ed51fd46b80c344c44f5262f5c18
Imran Ali <Imran.Ali@justice.gov.uk>
2023-11-22 13:28:36 +0000

    Revert "changed TestConfiguration to work with application.yaml so we can use defaults"
    This reverts commit 30fa2f4670795c006ee2513908fbb24936dde3c8.

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/config/TestConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/config/TestConfiguration.java
@@ -1,66 +1,61 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.config;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
-@ExtendWith(SpringExtension.class)
-@TestPropertySource("classpath:application.yaml")
-public final class TestConfiguration {
+public class TestConfiguration {
+
+    private static final Config config;
+
+    public static final String TEST_URL;
+
+    public static final long SCAN_DELAY;
+
+    public static final String S2S_URL;
+
+    public static final String S2S_NAME;
+
+    public static final String S2S_SECRET;
+
+    public static final boolean FLUX_FUNC_TEST;
+
+    public static final String STORAGE_ACCOUNT_URL;
+
+    public static final String STORAGE_ACCOUNT_NAME;
+
+    public static final String STORAGE_ACCOUNT_KEY;
+
+    public static final String STORAGE_CONTAINER_NAME;
+
+    public static final String PROCESSED_ENVELOPES_QUEUE_CONN_STRING;
+
+    public static final String PROCESSED_ENVELOPES_QUEUE_NAME;
+
+    public static final boolean JMS_ENABLED;
+
+    static {
+        config = ConfigFactory.load();
+        TEST_URL = config.getString("test-url");
+        SCAN_DELAY = Long.parseLong(config.getString("test-scan-delay"));
+        S2S_URL = config.getString("test-s2s-url");
+        S2S_NAME = config.getString("test-s2s-name");
+        S2S_SECRET = config.getString("test-s2s-secret");
+        FLUX_FUNC_TEST = config.getBoolean("flux-func-test");
+        STORAGE_ACCOUNT_URL = config.getString("test-storage-account-url");
+        STORAGE_ACCOUNT_NAME = config.getString("test-storage-account-name");
+        STORAGE_ACCOUNT_KEY = config.getString("test-storage-account-key");
+        STORAGE_CONTAINER_NAME = config.getString("test-storage-container-name");
+        JMS_ENABLED = config.getBoolean("jms-enabled");
+
+        PROCESSED_ENVELOPES_QUEUE_CONN_STRING = String.format(
+            "Endpoint=sb://%s.servicebus.windows.net;SharedAccessKeyName=%s;SharedAccessKey=%s;",
+            config.getString("processed-envelopes-queue-namespace"),
+            config.getString("processed-envelopes-queue-access-key-name"),
+            config.getString("processed-envelopes-queue-access-key")
+        );
+        PROCESSED_ENVELOPES_QUEUE_NAME = config.getString("processed-envelopes-queue-name");
+    }
 
     private TestConfiguration() {
     }
-
-    @Value("${test-url}")
-    public static String TEST_URL;
-
-    @Value("${test-scan-delay}")
-    public static long SCAN_DELAY;
-
-    @Value("${test-s2s-url}")
-    public static String S2S_URL;
-
-    @Value("${test-s2s-name}")
-    public static String S2S_NAME;
-
-    @Value("${test-s2s-secret}")
-    public static String S2S_SECRET;
-
-    @Value("${flux-func-test}")
-    public static boolean FLUX_FUNC_TEST;
-
-    @Value("${test-storage-account-url}")
-    public static String STORAGE_ACCOUNT_URL;
-
-    @Value("${test-storage-account-name}")
-    public static String STORAGE_ACCOUNT_NAME;
-
-    @Value("${test-storage-account-key}")
-    public static String STORAGE_ACCOUNT_KEY;
-
-    @Value("${test-storage-container-name}")
-    public static String STORAGE_CONTAINER_NAME;
-
-    @Value("${processed-envelopes-queue-namespace}")
-    public static String PROCESSED_ENVELOPES_QUEUE_NAMESPACE;
-
-    @Value("${processed-envelopes-queue-access-key-name}")
-    public static String PROCESSED_ENVELOPES_QUEUE_ACCESS_KEY_NAME;
-
-    @Value("${processed-envelopes-queue-access-key}")
-    public static String PROCESSED_ENVELOPES_QUEUE_ACCESS_KEY;
-
-    public static String PROCESSED_ENVELOPES_QUEUE_CONN_STRING = String.format(
-        "Endpoint=sb://%s.servicebus.windows.net;SharedAccessKeyName=%s;SharedAccessKey=%s;",
-        PROCESSED_ENVELOPES_QUEUE_NAMESPACE,
-        PROCESSED_ENVELOPES_QUEUE_ACCESS_KEY_NAME,
-        PROCESSED_ENVELOPES_QUEUE_ACCESS_KEY
-    );
-
-    @Value("${processed-envelopes-queue-name}")
-    public static String PROCESSED_ENVELOPES_QUEUE_NAME;
-
-    @Value("${jms-enabled}")
-    public static boolean JMS_ENABLED;
 }
