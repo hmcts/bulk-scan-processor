b46facb049c75af8dc0d0ade748ad80e4ff0c38d
Josh <joshblackmoor@users.noreply.github.com>
2024-04-12 10:08:57 +0100

    Remove unused secrets (#3266)

--- infrastructure/tf-db-flexible-secrets.tf
+++ infrastructure/tf-db-flexible-secrets.tf
@@ -1,6 +1,5 @@
 # Postgres 15 flexible server store secrets in key vault
 locals {
-  flexible_secret_prefix         = "${var.component}-POSTGRES-FLEXIBLE"
   flexible_secret_prefix_staging = "${var.component}-staging-db-flexible"
   standard_secret_prefix = "${var.component}-POSTGRES"
 
@@ -52,18 +51,6 @@ locals {
       value       = local.db_name
     }
   ]: []
-}
-
-resource "azurerm_key_vault_secret" "flexible_secret" {
-  for_each     = { for secret in local.flexible_secrets : secret.name_suffix => secret }
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-  name         = "${local.flexible_secret_prefix}-${each.value.name_suffix}"
-  value        = each.value.value
-  tags = merge(var.common_tags, {
-    "source" : "${var.component} PostgreSQL"
-  })
-  content_type    = ""
-  expiration_date = timeadd(timestamp(), "17520h")
 }
 
 resource "azurerm_key_vault_secret" "flexible_secret_standard_format" {
