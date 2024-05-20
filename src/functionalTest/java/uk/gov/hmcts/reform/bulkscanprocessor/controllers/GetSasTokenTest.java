77dc1e92590838439a3e7c325b8e35bda43e38ca
Ruth Bovell <ruth.bovell@justice.gov.uk>
2024-05-20 11:00:52 +0100

    FACT 1698 Renovate Dependency Updates (#3270)
    * Update dependency commons-io:commons-io to v2.16.1
    
    * Update dependency org.apache.commons:commons-lang3 to v3.14.0
    
    * Update plugin org.springframework.boot to v3.2.5
    
    * Update dependency com.azure:azure-storage-blob to v12.25.4
    
    * - manually updates chart java
    
    * Bumping chart version/ fixing aliases
    
    * - downgrade pact version
    
    * - updates test to check for new latest azure storage version
    
    * - updates functional tests test to check for new latest azure storage version
    
    * - manually upgrade to flyway v10
    
    * - manually upgrade gradle
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/GetSasTokenTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/GetSasTokenTest.java
@@ -137,7 +137,7 @@ public class GetSasTokenTest extends BaseFunctionalTest  {
         Date tokenExpiry = DateUtil.parseDatetime(queryParams.get("se"));
         assertThat(tokenExpiry).isNotNull();
         assertThat(queryParams.get("sig")).isNotNull(); //this is a generated hash of the resource string
-        assertThat(queryParams.get("sv")).contains("2021-12-02"); //azure api version is latest
+        assertThat(queryParams.get("sv")).contains("2023-11-03"); //azure api version is latest
         assertThat(queryParams.get("sp")).contains("wl"); //access permissions(write-w,list-l)
     }
 
