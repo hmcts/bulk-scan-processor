2e0b53ad0ff73658c5c6f36f45d893cef86ab83a
Josh Blackmoor <joshblackmoor01@icloud.com>
2024-06-19 13:09:33 +0100

    removed stg db

--- infrastructure/tf-db-flexible-secrets.tf
+++ infrastructure/tf-db-flexible-secrets.tf
@@ -1,6 +1,5 @@
 # Postgres 15 flexible server store secrets in key vault
 locals {
-  flexible_secret_prefix_staging = "${var.component}-staging-db-flexible"
   standard_secret_prefix = "${var.component}-POSTGRES"
 
   flexible_secrets = [
@@ -25,50 +24,12 @@ locals {
       value       = local.db_name
     }
   ]
-
-  flexible_secrets_staging = var.env == "aat" ? [
-    {
-      name_suffix = "pass"
-      value       = module.postgresql-staging[0].password
-      count       = var.num_staging_dbs
-    },
-    {
-      name_suffix = "host"
-      value       = module.postgresql-staging[0].fqdn
-      count       = var.num_staging_dbs
-    },
-    {
-      name_suffix = "user"
-      value       = module.postgresql-staging[0].username
-      count       = var.num_staging_dbs
-    },
-    {
-      name_suffix = "port"
-      value       = "5432"
-    },
-    {
-      name_suffix = "database"
-      value       = local.db_name
-    }
-  ]: []
 }
 
 resource "azurerm_key_vault_secret" "flexible_secret_standard_format" {
   for_each     = { for secret in local.flexible_secrets : secret.name_suffix => secret }
   key_vault_id = data.azurerm_key_vault.key_vault.id
   name         = "${local.standard_secret_prefix}-${each.value.name_suffix}"
-  value        = each.value.value
-  tags = merge(var.common_tags, {
-    "source" : "${var.component} PostgreSQL"
-  })
-  content_type    = ""
-  expiration_date = timeadd(timestamp(), "17520h")
-}
-
-resource "azurerm_key_vault_secret" "flexible_secret_staging" {
-  for_each     = { for secret in local.flexible_secrets_staging : secret.name_suffix => secret }
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-  name         = "${local.flexible_secret_prefix_staging}-${each.value.name_suffix}"
   value        = each.value.value
   tags = merge(var.common_tags, {
     "source" : "${var.component} PostgreSQL"
