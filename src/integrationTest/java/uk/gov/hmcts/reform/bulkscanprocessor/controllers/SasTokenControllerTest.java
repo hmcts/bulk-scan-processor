ace8e58a50e8dfd6a0cabd684d4b293addc794ed
Ruth Bovell <ruthb111@outlook.com>
2024-09-18 17:25:44 +0100

    - updates test to check for latest azure storage version

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/SasTokenControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/controllers/SasTokenControllerTest.java
@@ -67,7 +67,7 @@ public class SasTokenControllerTest {
 
         assertThat(queryParams.get("sig")).isNotNull();//this is a generated hash of the resource string
         assertThat(queryParams.get("se")).startsWith(currentDate);//the expiry date/time for the signature
-        assertThat(queryParams.get("sv")).contains("2023-11-03");//azure api version is latest
+        assertThat(queryParams.get("sv")).contains("2024-08-04");//azure api version is latest
         assertThat(queryParams.get("sp")).contains("wl");//access permissions(write-w,list-l)
     }
 }
