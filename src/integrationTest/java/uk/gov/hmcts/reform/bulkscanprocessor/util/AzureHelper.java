bf1531a2818ff51b4c882bc6e1c2cabe4f76f894
Ruth Bovell <ruthb111@outlook.com>
2024-09-24 08:35:25 +0100

    - reverts azure blob storage version

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/util/AzureHelper.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanprocessor/util/AzureHelper.java
@@ -5,7 +5,7 @@ public final class AzureHelper {
     private AzureHelper() {
     }
 
-    public static final String AZURE_TEST_CONTAINER = "hmctspublic.azurecr.io/imported/azure-storage/azurite:3.31.0";
+    public static final String AZURE_TEST_CONTAINER = "hmctspublic.azurecr.io/imported/azure-storage/azurite:3.29.0";
     public static final String EXTRACTION_HOST = "azurite";
     public static final int CONTAINER_PORT = 10000;
     public static final String CONTAINER_NAME = "bulkscan";
