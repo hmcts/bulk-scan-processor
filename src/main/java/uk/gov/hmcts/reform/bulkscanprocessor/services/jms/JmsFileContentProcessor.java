1553edc980f92b2a6fed7ef58e2836286f50cada
Ruth Bovell <ruthb111@outlook.com>
2024-11-19 17:11:35 +0000

    - adds case reference to handling envelope logs

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/jms/JmsFileContentProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/services/jms/JmsFileContentProcessor.java
@@ -71,10 +71,13 @@ public class JmsFileContentProcessor {
         String zipFilename,
         String containerName
     ) {
+
+        String caseReference = null;
         try {
             ZipFileContentDetail zipDetail = zipFileProcessor.getZipContentDetail(zis, zipFilename);
 
             InputEnvelope inputEnvelope = envelopeProcessor.parseEnvelope(zipDetail.getMetadata(), zipFilename);
+            caseReference = inputEnvelope.caseNumber;
 
             log.info(
                 "Parsed envelope. File name: {}. Container: {}. Payment DCNs: {}. Document DCNs: {}, caseNumber {}",
@@ -93,21 +96,25 @@ public class JmsFileContentProcessor {
             );
         } catch (PaymentsDisabledException ex) {
             log.error(
-                "Rejected file {} from container {} - Payments processing is disabled", zipFilename, containerName
+                "Rejected file {} from container {}, Case reference (if any): {} - Payments processing is disabled",
+                zipFilename, containerName, caseReference
             );
             Long eventId = createEvent(FILE_VALIDATION_FAILURE, containerName, zipFilename, ex.getMessage());
             fileRejector.handleInvalidBlob(eventId, containerName, zipFilename, ex);
         } catch (ServiceDisabledException ex) {
             log.error(
-                "Rejected file {} from container {} - Service is disabled", zipFilename, containerName
+                "Rejected file {} from container {}, Case reference (if any): {} - Service is disabled",
+                zipFilename, containerName, caseReference
             );
             Long eventId = createEvent(DISABLED_SERVICE_FAILURE, containerName, zipFilename, ex.getMessage());
             fileRejector.handleInvalidBlob(eventId, containerName, zipFilename, ex);
         }  catch (PreviouslyFailedToUploadException ex) {
-            log.warn("Rejected file {} from container {} - failed previously", zipFilename, containerName, ex);
+            log.warn("Rejected file {} from container {}, Case reference (if any): {} - failed previously",
+                     zipFilename, containerName, caseReference, ex);
             createEvent(DOC_UPLOAD_FAILURE, containerName, zipFilename, ex.getMessage());
         } catch (EnvelopeRejectionException ex) {
-            log.warn("Rejected file {} from container {} - invalid", zipFilename, containerName, ex);
+            log.warn("Rejected file {} from container {}, Case reference (if any): {} - invalid",
+                     zipFilename, containerName, caseReference, ex);
             Long eventId = createEvent(FILE_VALIDATION_FAILURE, containerName, zipFilename, ex.getMessage());
             fileRejector.handleInvalidBlob(eventId, containerName, zipFilename, ex);
         } catch (Exception ex) {
