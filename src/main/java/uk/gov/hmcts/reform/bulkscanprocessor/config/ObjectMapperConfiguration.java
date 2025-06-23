b539160ab2083b0325d880c428d438a70aacfbcc
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-06-23 08:31:51 +0100

    added time module

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ObjectMapperConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ObjectMapperConfiguration.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.bulkscanprocessor.config;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class ObjectMapperConfiguration {
     public ObjectMapper objectMapper() {
         return JsonMapper
             .builder()
+            .addModule(new JavaTimeModule())
             .disable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT)
             .build();
     }
