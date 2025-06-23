32627b05b31587697b8ff7f2262ddbf75713ef02
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-06-23 08:44:53 +0100

    Updated om cconfig

--- src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ObjectMapperConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanprocessor/config/ObjectMapperConfiguration.java
@@ -1,9 +1,13 @@
 package uk.gov.hmcts.reform.bulkscanprocessor.config;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +19,9 @@ public class ObjectMapperConfiguration {
         return JsonMapper
             .builder()
             .addModule(new JavaTimeModule())
+            .addModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES))
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .disable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT)
             .build();
     }
